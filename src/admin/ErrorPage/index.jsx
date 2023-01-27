93e68f406ee3c2bf86d337f45465b4fa563fa77d
Leo Wong <leoelicos@gmail.com>
2023-01-27 17:17:02 +1100

    Error page for unexpected errors

---
+++ src/admin/ErrorPage/index.jsx
@@ -0,0 +1,18 @@
+import { useRouteError } from 'react-router-dom'
+
+export default function ErrorPage() {
+  const error = useRouteError()
+  console.error(error)
+
+  return (
+    <div
+      id='error-page'
+      style={{ background: 'black', color: 'white', width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  )
+}

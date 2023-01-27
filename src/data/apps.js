686303daeb58fef71d9eb824b9fef4e8f2e4dc27
Leo Wong <leoelicos@gmail.com>
2023-01-27 17:18:26 +1100

    Export apps as objects

---
+++ src/data/apps.js
@@ -0,0 +1,85 @@
+import AboutMe from '../components/01__about-me'
+import LandingPage from '../components/02__landing-page'
+import CodeRefactor from '../components/03__code-refactor'
+import CSSSnippetCheatsheet from '../components/04__css-snippet-cheatsheet'
+import ProfessionalPortfolio from '../components/05__professional-portfolio'
+import RockPaperScissors from '../components/06__rock-paper-scissors'
+import PasswordGenerator from '../components/07__password-generator'
+import WordGuess from '../components/08__word-guess'
+import CodeQuiz from '../components/09__code-quiz'
+import ProjectTracker from '../components/10__project-tracker'
+import DayPlanner from '../components/11__day-planner'
+import LibrarySearchTool from '../components/12__library-search-tool'
+import WeatherDashboard from '../components/13__weather-dashboard'
+import Moovee from '../components/14__moovee'
+import GoodReadmeGenerator from '../components/15__good-readme-generator'
+import HTMLGenerator from '../components/16__html-generator'
+import TeamProfileGenerator from '../components/17__team-profile-generator'
+import CommandLineWordGuess from '../components/18__command-line-word-guess'
+import NoteTaker from '../components/19__note-taker'
+import DiagnosticRoutes from '../components/20__diagnostic-routes'
+import EmployeeManagementSystem from '../components/21__employee-management-system'
+import MovieDatabase from '../components/22__movie-database'
+import ECommerceBackEnd from '../components/23__ecommerce-back-end'
+import TripsDatabase from '../components/24__trips-database'
+import TechBlog from '../components/25__tech-blog'
+import CrowdfundingApp from '../components/26__crowdfunding-app'
+import JustKidding from '../components/27__just-kidding'
+import TechnicalInterview from '../components/28__technical-interview'
+import RegexTutorial from '../components/29__regex-tutorial'
+import StudentStatisticsBackEnd from '../components/30__student-statistics-back-end'
+import SocialNetworkAPI from '../components/31__social-network-api'
+import ContactDirectoryApp from '../components/32__contact-directory-app'
+import TextEditor from '../components/33__text-editor'
+import BucketList from '../components/34__bucket-list'
+import ReactPortfolio from '../components/35__react-portfolio'
+import TechMatchupApp from '../components/36__tech-matchup-app'
+import BookSearchEngine from '../components/37__book-search-engine'
+import ReduxRefactor from '../components/38__redux-refactor'
+import ReduxStore from '../components/39__redux-store'
+import Ingre from '../components/40__ingre'
+
+const apps = [
+  //
+  { name: 'About Me', link: '01-about-me', element: <AboutMe /> },
+  { name: 'Landing Page', link: '02-landing-page', element: <LandingPage /> },
+  { name: 'Code Refactor', link: '03-code-refactor', element: <CodeRefactor /> },
+  { name: 'CSS Snippet Cheatsheet', link: '04-css-snippet-cheatsheet', element: <CSSSnippetCheatsheet /> },
+  { name: 'Professional Portfolio', link: '05-professional-portfolio', element: <ProfessionalPortfolio /> },
+  { name: 'Rock Paper Scissors', link: '06-rock-paper-scissors', element: <RockPaperScissors /> },
+  { name: 'Password Generator', link: '07-password-generator', element: <PasswordGenerator /> },
+  { name: 'Word Guess', link: '08-word-guess', element: <WordGuess /> },
+  { name: 'Code Quiz', link: '09-code-quiz', element: <CodeQuiz /> },
+  { name: 'Project Tracker', link: '10-project-tracker', element: <ProjectTracker /> },
+  { name: 'Day Planner', link: '11-day-planner', element: <DayPlanner /> },
+  { name: 'Library Search Tool', link: '12-library-search-tool', element: <LibrarySearchTool /> },
+  { name: 'Weather Dashboard', link: '13-weather-dashboard', element: <WeatherDashboard /> },
+  { name: 'Moovee', link: '14-moovee', element: <Moovee /> },
+  { name: 'Good Readme Generator', link: '15-good-readme-generator', element: <GoodReadmeGenerator /> },
+  { name: 'HTML Generator', link: '16-html-generator', element: <HTMLGenerator /> },
+  { name: 'Team Profile Generator', link: '17-team-profile-generator', element: <TeamProfileGenerator /> },
+  { name: 'Command Line Word Guess', link: '18-command-line-word-guess', element: <CommandLineWordGuess /> },
+  { name: 'Note Taker', link: '19-note-taker', element: <NoteTaker /> },
+  { name: 'Diagnostic Routes', link: '20-diagnostic-routes', element: <DiagnosticRoutes /> },
+  { name: 'Employee Management System', link: '21-employee-management-system', element: <EmployeeManagementSystem /> },
+  { name: 'Movie Database', link: '22-movie-database', element: <MovieDatabase /> },
+  { name: 'ECommerce Back End', link: '23-ecommerce-back-end', element: <ECommerceBackEnd /> },
+  { name: 'Trips Database', link: '24-trips-database', element: <TripsDatabase /> },
+  { name: 'Tech Blog', link: '25-tech-blog', element: <TechBlog /> },
+  { name: 'Crowdfunding App', link: '26-crowdfunding-app', element: <CrowdfundingApp /> },
+  { name: 'Just Kidding', link: '27-just-kidding', element: <JustKidding /> },
+  { name: 'Technical Interview', link: '28-technical-interview', element: <TechnicalInterview /> },
+  { name: 'Regex Tutorial', link: '29-regex-tutorial', element: <RegexTutorial /> },
+  { name: 'Student Statistics Back End', link: '30-student-statistics-back-end', element: <StudentStatisticsBackEnd /> },
+  { name: 'Social Network API', link: '31-social-network-api', element: <SocialNetworkAPI /> },
+  { name: 'Contact Directory App', link: '32-contact-directory-app', element: <ContactDirectoryApp /> },
+  { name: 'Text Editor', link: '33-text-editor', element: <TextEditor /> },
+  { name: 'Bucket List', link: '34-bucket-list', element: <BucketList /> },
+  { name: 'React Portfolio', link: '35-react-portfolio', element: <ReactPortfolio /> },
+  { name: 'Tech Matchup App', link: '36-tech-matchup-app', element: <TechMatchupApp /> },
+  { name: 'Book Search Engine', link: '37-book-search-engine', element: <BookSearchEngine /> },
+  { name: 'Redux Refactor', link: '38-redux-refactor', element: <ReduxRefactor /> },
+  { name: 'Redux Store', link: '39-redux-store', element: <ReduxStore /> },
+  { name: 'Ingre', link: '40-ingre', element: <Ingre /> }
+]
+export default apps

223ebab9b64f06f694f053ffceeb7bf882d41c63
Leo Wong <leoelicos@gmail.com>
2023-01-28 21:40:30 +1100

    Extract data to external file

---
+++ src/components/03__code-refactor/data/articles.js
@@ -0,0 +1,28 @@
+import ImageArticle1 from '../images/search-engine-optimization.jpg'
+import ImageArticle2 from '../images/online-reputation-management.jpg'
+import ImageArticle3 from '../images/social-media-marketing.jpg'
+
+const articles = [
+  {
+    id: 'search-engine-optimization',
+    imgSrc: ImageArticle1,
+    imgAlt: 'a mind map of SEO applications',
+    heading: 'Search Engine Optimization',
+    paragraph: 'The dominance of mobile internet use means that users are searching for the right business as they travel, shop, or sit on their couch at home. Search Engine Optimization (SEO) allows you to increase your visibility and find the right customers for your business.'
+  },
+  {
+    id: 'online-reputation-management',
+    imgSrc: ImageArticle2,
+    imgAlt: "a graph of a company's reputation increasing over time",
+    heading: 'Online Reputation Management',
+    paragraph: 'The web is full of opinions, and some of these can be negative. Social media allows anyone with an internet connection to say whatever they want about your business. Online Reputation Management gives you the control over what potential customers see when they search for your business.'
+  },
+  {
+    id: 'social-media-marketing',
+    imgSrc: ImageArticle3,
+    imgAlt: 'a picture of SEO staff collaborating together in various ways',
+    heading: 'Social Media Marketing',
+    paragraph: 'Social media continues to have a sizable influence on buying habits. Social media marketing helps you determine which platforms are suited to your brand, using analytics to find the right markets and increase your lead generation.'
+  }
+]
+export default articles

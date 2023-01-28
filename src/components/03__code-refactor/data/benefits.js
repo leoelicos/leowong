a89387cdde56041fed5480f6bb6644e88779382b
Leo Wong <leoelicos@gmail.com>
2023-01-28 21:40:35 +1100

    Extract data to external file

---
+++ src/components/03__code-refactor/data/benefits.js
@@ -0,0 +1,28 @@
+import ImageBenefit1 from '../images/lead-generation.png'
+import ImageBenefit2 from '../images/brand-awareness.png'
+import ImageBenefit3 from '../images/cost-management.png'
+
+const benefits = [
+  {
+    id: 'benefit-lead',
+    imgSrc: ImageBenefit1,
+    imgAlt: 'a icon showing money coming from a good idea',
+    heading: 'Lead Generation',
+    paragraph: 'Inbound strategies for lead generation require less work for your business, bringing customers directly to your website.'
+  },
+  {
+    id: 'benefit-brand',
+    imgSrc: ImageBenefit2,
+    imgAlt: 'a lightbulb icon showing people becoming aware of a good idea',
+    heading: 'Brand Awareness',
+    paragraph: 'Users find your business through paid and organic searches, increasing the search ranking and visibility for your business.'
+  },
+  {
+    id: 'benefit-cost',
+    imgSrc: ImageBenefit3,
+    imgAlt: 'a cog icon representing different costs being managed together',
+    heading: 'Cost Management',
+    paragraph: 'As the search ranking for your business increases, your advertising costs decrease, and you no longer need to advertise your page.'
+  }
+]
+export default benefits

31f9f5ebe22886d153de4065763a5d81ddcf1e65
Leo Wong <leoelicos@gmail.com>
2023-01-29 17:13:35 +1100

    Images for projects and backgrounds

---
+++ src/components/05__professional-portfolio/images/index.js
@@ -0,0 +1,35 @@
+import Avatar from './avatar.jpg'
+import BgCar from './bg/car.png'
+import BgMotorway from './bg/motorway.png'
+import BgMountain from './bg/mountain.png'
+import BgRoad from './bg/road.png'
+import IconFavicon from './icon/favicon.svg'
+import IconGitHub from './icon/gh.png'
+import IconLinkedIn from './icon/li.png'
+import Project21EMS from './project/ems.jpg'
+import Project40Ingre from './project/ingre.jpg'
+import Project27JustKidding from './project/jk.jpg'
+import Project08WordGame from './project/kiddle.jpg'
+import Project14Moovee from './project/moovee.png'
+import Project25Submitted from './project/submitted.jpg'
+import Project09CodeQuiz from './project/sytycj.jpg'
+import Project13WeatherDashboard from './project/wwag.jpg'
+
+export {
+  Avatar,
+  BgCar,
+  BgMotorway,
+  BgMountain,
+  BgRoad,
+  IconFavicon,
+  IconGitHub,
+  IconLinkedIn,
+  Project08WordGame,
+  Project09CodeQuiz,
+  Project13WeatherDashboard,
+  Project14Moovee,
+  Project21EMS,
+  Project25Submitted,
+  Project27JustKidding,
+  Project40Ingre //
+}

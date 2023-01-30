d16e859e262091ab43b2f6aaaf79c02dd2646a53
Leo Wong <leoelicos@gmail.com>
2023-01-30 21:57:00 +1100

    Implement with useRef, useState, setInterval

--- src/components/06__rock-paper-scissors/index.jsx
+++ src/components/06__rock-paper-scissors/index.jsx
@@ -1,91 +1,90 @@
+import { faHandPaper, faHandRock, faHandScissors } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useRef, useState } from 'react'
+import './style/style.css'
+const choices = [
+  { id: 'rock', icon: faHandRock },
+  { id: 'paper', icon: faHandPaper },
+  { id: 'scissors', icon: faHandScissors }
+]
+const judge = (playerChoice, computerChoice) =>
+  computerChoice === playerChoice
+    ? 'draw'
+    : (computerChoice === 'rock' && playerChoice === 'scissors') || //
+      (computerChoice === 'paper' && playerChoice === 'rock') ||
+      (computerChoice === 'scissors' && playerChoice === 'paper')
+    ? 'computer'
+    : 'player'
 export default function RockPaperScissors() {
+  const [playerChoice, setPlayerChoice] = useState(undefined)
+  const [computerChoice, setComputerChoice] = useState(undefined)
+  const [win, setWin] = useState(undefined)
+  const [tally, setTally] = useState({ wins: 0, losses: 0, tally: 0 })
+  const rotateTimer = useRef(null)
+  const handleChoice = (choice) => {
+    setPlayerChoice(choice)
+
+    rotateTimer.current = setInterval(() => {
+      setComputerChoice((prev) => (prev === undefined || prev === 'scissors' ? 'rock' : prev === 'rock' ? 'paper' : 'scissors'))
+    }, 100)
+
+    setTimeout(() => {
+      clearInterval(rotateTimer.current)
+      let computerChoice = choices[Math.floor(Math.random() * 3)].id
+      setComputerChoice(computerChoice)
+      let win = undefined
+      if (computerChoice === choice) {
+        win = 'draw'
+      } else if (
+        (computerChoice === 'rock' && playerChoice === 'scissors') || //
+        (computerChoice === 'paper' && playerChoice === 'rock') ||
+        (computerChoice === 'scissors' && playerChoice === 'paper')
+      )
+        win = 'computer'
+      else win = 'player'
+
+      setWin(win)
+    }, 1000)
+  }
   return (
     <div className='app-06'>
-      <div className='body'></div>
+      <div className='body'>
+        <section className='instruction'>Choose wisely</section>
+        <section className='game-container'>
+          {choices.map(({ icon, id }, i) => (
+            <button
+              key={id}
+              className={id === playerChoice ? 'active' : ''}
+              onClick={() => {
+                handleChoice(id)
+              }}>
+              <div className='icon-wrapper'>
+                <FontAwesomeIcon icon={icon} />
+              </div>
+            </button>
+          ))}
+        </section>
+        <section className='game-container'>
+          {choices.map(({ icon, id }, i) => (
+            <button
+              key={id}
+              className={`computer ${id === computerChoice ? 'active' : ''}`}>
+              <div className='icon-wrapper'>
+                <FontAwesomeIcon icon={icon} />
+              </div>
+            </button>
+          ))}
+        </section>
+        <section className='result'>
+          {win === 'player' //
+            ? 'You win'
+            : win === 'computer'
+            ? 'You lose'
+            : win === 'draw'
+            ? 'Draw'
+            : ''}
+        </section>
+      </div>
     </div>
   )
-}
-function f() {
-  // rock, paper, scissors
-
-  const rock = 'R'
-  const paper = 'P'
-  const scissors = 'S'
-
-  // tallies the player's wins
-  var player = {
-    wins: 0,
-    ties: 0,
-    losses: 0
-  }
-
-  // 0 = rock, 1 = paper, 2 = scissors
-  var gameChoices = [rock, paper, scissors]
-
-  window.alert('*********************\nRock-Paper-Scissors\n*********************\n\nRules:\nrock (R) beats scissors (S)\npaper (P) beats rock (R)\nscissors (S) beats paper (P)')
-
-  var keepPlaying = window.confirm('Play ?')
-  var checkValues = ['R', 'P', 'R']
-
-  while (keepPlaying === true) {
-    var playerChoice = window.prompt('To play, type R, P or S', 'R')
-
-    // player selects cancel
-    if (playerChoice == null) {
-      keepPlaying = false
-    }
-    // check data is either R, P or S. If not, try again.
-    else if (!checkValues.includes(playerChoice.toUpperCase())) {
-      window.alert('Invalid command. Please type R, P or S. Try again.')
-    }
-    // player wants to play
-    else {
-      playerChoice = playerChoice.toUpperCase()
-      var result = 'This game: '
-      result += 'You chose '
-      if (playerChoice === rock) {
-        result += 'rock'
-      } else if (playerChoice === paper) {
-        result += 'paper'
-      } else if (playerChoice === scissors) {
-        result += 'scissors'
-      }
-      result += '.\n'
-
-      var computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)]
-
-      result += 'Computer chose '
-      if (computerChoice === rock) {
-        result += 'rock'
-      } else if (computerChoice === paper) {
-        result += 'paper'
-      } else if (computerChoice === scissors) {
-        result += 'scissors'
-      }
-      result += '.\n'
-
-      // tie
-      if (playerChoice === computerChoice) {
-        player.ties++
-        result += 'Result = Tie.\n'
-      }
-
-      // player wins
-      else if ((playerChoice === rock && computerChoice === scissors) || (playerChoice === scissors && computerChoice === paper) || (playerChoice === paper && computerChoice === rock)) {
-        player.wins++
-        result += 'Result = You win!\n'
-      }
-
-      // player loses
-      else {
-        player.losses++
-        result += 'Result = You lose!\n'
-      }
-
-      // display stats and ask if they wanna play again
-      keepPlaying = window.confirm(result + '\nYour current standings:\n' + player.wins + ' wins, ' + player.ties + ' ties, ' + player.losses + ' losses.\n\nPlay another game?')
-    }
-  }
-
-  window.alert('Thanks for playing, bye now!')
 }

931971ce9fcf9985836f02cbe71746532a1d39e7
Leo Wong <leoelicos@gmail.com>
2023-01-30 23:37:00 +1100

    Starter HTML

--- src/components/07__password-generator/index.jsx
+++ src/components/07__password-generator/index.jsx
@@ -1,3 +1,32 @@
 export default function PasswordGenerator() {
-  return <div>PasswordGenerator</div>
+  return (
+    <div className='app-07'>
+      <div className='body'>
+        <div className='wrapper'>
+          <header>
+            <h1>Password Generator</h1>
+          </header>
+          <div className='card'>
+            <div className='card-header'>
+              <h2>Generate a Password</h2>
+            </div>
+            <div className='card-body'>
+              <textarea
+                readOnly
+                id='password'
+                placeholder='Your Secure Password'
+                aria-label='Generated Password'></textarea>
+            </div>
+            <div className='card-footer'>
+              <button
+                id='generate'
+                className='btn'>
+                Generate Password
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }

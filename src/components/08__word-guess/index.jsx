b00900d7bd94192e619afda221db315d055648df
Leo Wong <leoelicos@gmail.com>
2023-02-02 21:09:52 +1100

    Starter HTML

--- src/components/08__word-guess/index.jsx
+++ src/components/08__word-guess/index.jsx
@@ -1,3 +1,88 @@
 export default function WordGuess() {
-  return <div>WordGuess</div>
+  return (
+    <div className='app-08'>
+      <div className='body'>
+        <header>
+          <div className='logo'>
+            <img
+              src='./assets/images/elephantOrange.png'
+              alt='elephant logo'
+            />
+          </div>
+          <div>
+            <h1>KIDDLE</h1>
+          </div>
+        </header>
+
+        <main>
+          <section className='alarm-wrapper'>
+            <div className='alarm'>
+              0:<span id='secondsLeft'>10</span>
+            </div>
+          </section>
+          <div className='tile-wrapper'>
+            <div
+              className='tile'
+              id='tile0'>
+              _
+            </div>
+            <div
+              className='tile'
+              id='tile1'>
+              _
+            </div>
+            <div
+              className='tile'
+              id='tile2'>
+              _
+            </div>
+            <div
+              className='tile'
+              id='tile3'>
+              _
+            </div>
+          </div>
+          <section className='start-wrapper'>
+            <button
+              className='btn'
+              id='button-start-games'>
+              Start
+            </button>
+          </section>
+        </main>
+
+        <footer>
+          <section className='tally'>
+            <button id='button-tally-wins'>
+              <div id='tally-wins-label'>Wins:&nbsp;</div>
+              <div id='tally-wins-value'>0</div>
+            </button>
+
+            <button id='button-tally-losses'>
+              <div id='tally-losses-label'>Losses:&nbsp;</div>
+              <div id='tally-losses-value'>0</div>
+            </button>
+          </section>
+          <div
+            id='modal'
+            className='hide'>
+            <button id='closeModal'>X</button>
+            <p>Click start to play. Type the letters to guess the word before the timer hits 0!</p>
+          </div>
+          <section className='footer-buttons'>
+            <button
+              className='btn'
+              id='button-reset-games'>
+              Reset
+            </button>
+            <button
+              className='btn'
+              id='button-help'>
+              ?
+            </button>
+          </section>
+        </footer>
+      </div>
+    </div>
+  )
 }

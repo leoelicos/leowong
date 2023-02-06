2900deab74b6a5c799910348c294d1e25a3bc4a1
Leo Wong <leoelicos@gmail.com>
2023-02-07 02:32:18 +1100

    Add placeholder for no HighScores

--- src/components/09__code-quiz/pages/HighScores.jsx
+++ src/components/09__code-quiz/pages/HighScores.jsx
@@ -11,7 +11,7 @@ export default function HighScores({ goSplash, highScores, clearHighScores }) {
             <span id='hs-score'>Score</span>
           </button>
         </li>
-        {highScores &&
+        {highScores.length ? (
           highScores
             .sort((a, b) => b.score - a.score)
             .map(({ initials, score, timestamp }, i) => (
@@ -26,7 +26,24 @@ export default function HighScores({ goSplash, highScores, clearHighScores }) {
                   <span id='hs-score'>{score}</span>
                 </button>
               </li>
+            ))
+        ) : (
+          <>
+            {[1, 2, 3, 4].map((v) => (
+              <li
+                key={v}
+                className='btn-wrapper'>
+                <button
+                  id='hs-wrapper'
+                  className='btn'>
+                  <span id='hs-rank'>♾️</span>
+                  <span id='hs-initial'>♾️</span>
+                  <span id='hs-score'>♾️</span>
+                </button>
+              </li>
             ))}
+          </>
+        )}
       </ul>
       <button
         className='splash-start-button'

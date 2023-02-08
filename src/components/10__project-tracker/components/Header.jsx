e95c13c6c0e3f6b7a2220517d3a868b8803d95cd
Leo Wong <leoelicos@gmail.com>
2023-02-08 21:17:47 +1100

    Header function with dummy time

---
+++ src/components/10__project-tracker/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header({ timeString, dateString, handleClickAdd }) {
+  return (
+    <header class='jumbotron jumbotron-fluid custom-jumbotron'>
+      <div
+        class='container text-center'
+        id='hero'>
+        <h1>Project Tracker</h1>
+        <h2>
+          Time&ensp;<span id='time-display'>{timeString}</span>
+        </h2>
+        <h2>
+          Date&ensp;<span id='date-display'>{dateString}</span>
+        </h2>
+        <button onClick={handleClickAdd}>Add Project</button>
+      </div>
+    </header>
+  )
+}
+export default Header

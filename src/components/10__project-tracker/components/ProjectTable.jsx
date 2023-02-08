c7be018d59b632ad95c10854c81fe6d02cc746d7
Leo Wong <leoelicos@gmail.com>
2023-02-08 20:53:04 +1100

    Implement table view using antd Table

---
+++ src/components/10__project-tracker/components/ProjectTable.jsx
@@ -0,0 +1,42 @@
+import { Table } from 'antd'
+const { Column } = Table
+
+function ProjectTable({ data }) {
+  console.log(data)
+  return (
+    <Table dataSource={data}>
+      <Column
+        title='Project Name'
+        dataIndex='projectName'
+        key='projectName'
+      />
+      <Column
+        title='Project Type'
+        dataIndex='projectType'
+        key='projectType'
+      />
+      <Column
+        title='Hourly Rate'
+        dataIndex='hourlyRate'
+        key='hourlyRate'
+      />
+      <Column
+        title='Due Date'
+        dataIndex='dueDate'
+        key='dueDate'
+      />
+
+      <Column
+        title='Days until Due Date'
+        dataIndex='daysUntilDueDate'
+        key='daysUntilDueDate'
+      />
+      <Column
+        title='Potential total Earnings ($)'
+        dataIndex='potentialEarnings'
+        key='potentialEarnings'
+      />
+    </Table>
+  )
+}
+export default ProjectTable

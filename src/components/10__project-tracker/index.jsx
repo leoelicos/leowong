5b5c97fe507017f53c1da70e9b216a86881d8b79
Leo Wong <leoelicos@gmail.com>
2023-02-08 21:18:04 +1100

    Implement custom hook useTime

--- src/components/10__project-tracker/index.jsx
+++ src/components/10__project-tracker/index.jsx
@@ -1,6 +1,39 @@
 import ProjectTable from './components/ProjectTable'
+import Header from './components/Header'
 import dummyData from './data/dummydata'
-console.log({ dummyData })
+import { useEffect, useRef, useState } from 'react'
+
+const useTime = () => {
+  const [time, setTime] = useState(new Date().getTime())
+  const timer = useRef(null)
+
+  const incrementTime = () => {
+    setTime((prev) => prev + 1)
+  }
+  useEffect(() => {
+    clearInterval(timer.current)
+    timer.current = setInterval(incrementTime, 1000)
+  }, [])
+  const newTime = new Date(time)
+  const timeString = newTime.toLocaleTimeString([], { hour12: true })
+  const dateString = newTime.toLocaleDateString()
+  return [timeString, dateString]
+}
+
 export default function ProjectTracker() {
-  return <ProjectTable data={dummyData} />
+  const [modal, setModal] = useState(false)
+  const [timeString, dateString] = useTime()
+  const handleClickAdd = () => {
+    setModal(true)
+  }
+  return (
+    <main>
+      <Header
+        timeString={timeString}
+        dateString={dateString}
+        handleClickAdd={handleClickAdd}
+      />
+      <ProjectTable data={dummyData} />
+    </main>
+  )
 }

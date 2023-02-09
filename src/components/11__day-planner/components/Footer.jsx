aabc2c9855ba6bb0dc0e38f658f21b0d03763ce4
Leo Wong <leoelicos@gmail.com>
2023-02-09 21:21:38 +1100

    Starter footer

---
+++ src/components/11__day-planner/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <button className='toggle'>
+        <i className='material-icons bulb'>lightbulb</i>
+      </button>
+    </footer>
+  )
+}
+export default Footer

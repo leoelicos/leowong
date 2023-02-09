53fccf50c480aefb48439023b381541398416800
Leo Wong <leoelicos@gmail.com>
2023-02-10 00:54:53 +1100

    Implement time display with 'now' prop

--- src/components/11__day-planner/components/CustomHeader.jsx
+++ src/components/11__day-planner/components/CustomHeader.jsx
@@ -1,6 +1,18 @@
 import '../css/weather.css'
 
-function CustomHeader() {
+function CustomHeader({ now }) {
+  const hours = now.getHours()
+  const hoursForClock = hours % 12 || 12
+  const minutes = now.getMinutes()
+  const seconds = now.getSeconds()
+  const ampm = hours > 12 ? 'PM' : 'AM'
+  const degrees = (x, x1, x2, y1, y2) => {
+    return ((x - x1) * (y2 - y1)) / (x2 - x1) + y1
+  }
+  let hoursDegree = degrees(hoursForClock, 0, 12, 0, 360) || 360
+  let minutesDegree = degrees(minutes, 0, 60, 0, 360) || 360
+  let secondsDegree = degrees(seconds, 0, 60, 0, 360) || 360
+
   return (
     <header id='header'>
       <div className='header-date'>
@@ -18,12 +30,18 @@ function CustomHeader() {
       </div>
       <div className='clock-container'>
         <div className='clock'>
-          <div className='needle clock-hour'></div>
-          <div className='needle minute'></div>
-          <div className='needle second'></div>
+          <div
+            className='needle clock-hour'
+            style={{ transition: 'all 0s', transform: `translate(-50%, -100%) rotate(${hoursDegree}deg)` }}></div>
+          <div
+            className='needle minute'
+            style={{ transform: `translate(-50%, -100%) rotate(${minutesDegree}deg)` }}></div>
+          <div
+            className='needle second'
+            style={{ transform: `translate(-50%, -100%) rotate(${secondsDegree}deg)` }}></div>
           <div className='center-point'></div>
         </div>
-        <div className='time'></div>
+        <div className='time'>{`${hoursForClock === 0 ? 12 : hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`}</div>
       </div>
       <figure id='weather-container'>
         <img

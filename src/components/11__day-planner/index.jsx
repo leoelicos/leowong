8dda4f0f28c133d62443d984602dda0d186af3ad
Leo Wong <leoelicos@gmail.com>
2023-02-09 23:49:51 +1100

    Rename Header & Footer to CustomHeader & CustomFooter

--- src/components/11__day-planner/index.jsx
+++ src/components/11__day-planner/index.jsx
@@ -1,10 +1,35 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
+/* hooks */
+import { useState } from 'react'
+
+/* custom components */
+import CustomHeader from './components/CustomHeader'
+import CustomFooter from './components/CustomFooter'
 import AutoComplete from './components/AutoComplete'
+
 export default function DayPlanner() {
+  /* state */
+  const [toggle, setToggle] = useState(false)
+
+  /* event handlers */
+  const handleToggle = () => {
+    setToggle((prev) => {
+      console.log(`Setting toggle to ${!prev}`)
+      return !prev
+    })
+  }
+
   return (
-    <div className='app-11'>
-      <Footer />
+    <div className={`app-11 ${toggle ? 'dark' : ''}`}>
+      <div className='body'>
+        <CustomHeader />
+        <main className={`container ${toggle ? 'dark' : ''}`}>
+          <AutoComplete />
+        </main>
+        <CustomFooter
+          toggle={toggle}
+          handleToggle={handleToggle}
+        />
+      </div>
     </div>
   )
 }

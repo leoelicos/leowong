a943c878a3223d50df24197b2efbe6790de20519
Leo Wong <leoelicos@gmail.com>
2023-02-09 14:02:01 +1100

    Replace header with AntD header

---
+++ src/components/10__project-tracker/components/ProjectTrackerHeader.jsx
@@ -0,0 +1,25 @@
+import { Typography } from 'antd'
+
+const { Text, Title } = Typography
+function ProjectTrackerHeader({ time, handleClickAdd }) {
+  const d = new Date(time)
+  const timeString = d.toLocaleTimeString([], { hourCycle: 'h11', hour12: true, hour: 'numeric', minute: '2-digit', second: '2-digit' })
+  const dateString = d.toLocaleDateString(undefined, { day: 'numeric', month: 'short', year: 'numeric' })
+  return (
+    <div
+      className='container text-center'
+      id='hero'>
+      <Title level={1}>Project Tracker</Title>
+      <Title level={2}>
+        Time&ensp;<span id='time-display'>{timeString}</span>
+      </Title>
+      <Title level={2}>
+        Date&ensp;<span id='date-display'>{dateString}</span>
+      </Title>
+      <button onClick={handleClickAdd}>
+        <Text>Add Project</Text>
+      </button>
+    </div>
+  )
+}
+export default ProjectTrackerHeader

57f8fdd4ab3ac53cb5c07321372c2dcfb951acc1
Leo Wong <leoelicos@gmail.com>
2023-02-09 14:04:12 +1100

    Extract project type data

---
+++ src/components/10__project-tracker/data/projectTypes.js
@@ -0,0 +1,10 @@
+const projectTypes = [
+  //
+  'Web Application (Front End)',
+  'Web Application (Back End)',
+  'Web Application (Full Stack)',
+  'Mobile Application',
+  'Print Campaign',
+  'Digital Marketing Campaign'
+]
+export default projectTypes

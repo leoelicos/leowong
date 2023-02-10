00c232b001f07409e2f90e74ed6fa16688b118c0
Leo Wong <leoelicos@gmail.com>
2023-02-10 16:42:06 +1100

    Pass text state through props

--- src/components/11__day-planner/components/AutoComplete.jsx
+++ src/components/11__day-planner/components/AutoComplete.jsx
@@ -4,34 +4,25 @@ import { AutoComplete } from 'antd'
 import { useState } from 'react'
 import '../css/autocomplete.css'
 
-const App = () => {
-  const [value, setValue] = useState('')
+const App = ({ text, onTextChange }) => {
   const [options, setOptions] = useState([])
   const onSearch = (searchText) => {
     const matches = searchText.match(/\w+/gm)
     setOptions(!matches ? [] : duties.filter((duty) => matches.some((match) => duty.value.includes(match))))
   }
-  const onSelect = (data) => {
-    console.log('onSelect', data)
-  }
-  const onChange = (data) => {
-    setValue(data)
-  }
+
   return (
-    <>
-      <AutoComplete
-        className='autocomplete'
-        value={value}
-        options={options}
-        style={{
-          width: 200
-        }}
-        onSelect={onSelect}
-        onSearch={onSearch}
-        onChange={onChange}
-        placeholder='...'
-      />
-    </>
+    <AutoComplete
+      className='autocomplete'
+      value={text}
+      options={options}
+      style={{
+        width: 200
+      }}
+      onSearch={onSearch}
+      onChange={onTextChange}
+      placeholder='...'
+    />
   )
 }
 export default App

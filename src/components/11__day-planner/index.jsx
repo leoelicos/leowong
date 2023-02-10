e0d5007b38b7c6ea41fb5176c4dabeaed0d72eea
Leo Wong <leoelicos@gmail.com>
2023-02-11 05:55:11 +1100

    hooks manage external logic

--- src/components/11__day-planner/index.jsx
+++ src/components/11__day-planner/index.jsx
@@ -1,17 +1,30 @@
 /* hooks */
 import { useEffect, useState } from 'react'
+import useTime from './hooks/useTime'
+import useWeather from './hooks/useWeather'
+import useData from './hooks/useData'
+import useFavicon from '../../hooks/useFavicon'
+import useTitle from '../../hooks/useTitle'
 
 /* custom components */
 import CustomHeader from './components/CustomHeader'
 import CustomFooter from './components/CustomFooter'
-import AutoComplete from './components/AutoComplete'
+import TimeBlock from './components/TimeBlock'
 
-import useTime from './hooks/useTime'
+/* style */
+import './css/index.css'
 
 export default function DayPlanner() {
   /* state */
   const [toggle, setToggle] = useState(false)
+
+  /* hooks */
+  const { schedule, addToSchedule } = useData()
   const now = useTime()
+  const weather = useWeather()
+  useFavicon('favicons/11-favicon.png')
+  useTitle(now.toLocaleDateString('en-US', { weekday: 'long' }))
+
   /* event handlers */
   const handleToggle = () => {
     setToggle((prev) => {
@@ -21,19 +34,26 @@ export default function DayPlanner() {
   }
 
   /* effect */
-  useEffect(() => {
-    document.querySelector('#favicon').setAttribute('href', 'favicons/11-favicon.png')
-    return () => {
-      document.querySelector('#favicon').setAttribute('href', 'favicons/00-favicon.png')
-    }
-  }, [])
 
   return (
     <div className={`app-11 ${toggle ? 'dark' : ''}`}>
       <div className='body'>
-        <CustomHeader now={now} />
+        <CustomHeader
+          now={now}
+          weather={weather}
+        />
         <main className={`container ${toggle ? 'dark' : ''}`}>
-          <AutoComplete />
+          {schedule?.map((v) => {
+            return (
+              <TimeBlock
+                key={v.time}
+                time={v.time}
+                initialText={v.text}
+                addToSchedule={addToSchedule}
+                relative={now.getHours() === v.time ? 'present' : now.getHours() > v.time ? 'past' : 'future'}
+              />
+            )
+          }) || null}
         </main>
         <CustomFooter
           toggle={toggle}

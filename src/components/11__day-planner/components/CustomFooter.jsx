9ce4a3084c768f20ce7790c7a65017092e91ef19
Leo Wong <leoelicos@gmail.com>
2023-02-11 05:51:55 +1100

    Replace lightbulb with conditional icons

--- src/components/11__day-planner/components/CustomFooter.jsx
+++ src/components/11__day-planner/components/CustomFooter.jsx
@@ -1,18 +1,20 @@
-import { faLightbulb } from '@fortawesome/free-regular-svg-icons'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import '../css/toggle.css'
-
-function CustomFooter({ handleToggle }) {
+function CustomFooter({ toggle, handleToggle }) {
   return (
     <footer>
       <button
         className='toggle'
         onClick={handleToggle}>
-        <FontAwesomeIcon
-          className='bulb'
-          icon={faLightbulb}
-        />
+        {toggle ? (
+          <FontAwesomeIcon
+            className='bulb'
+            icon={faSun}
+          />
+        ) : (
+          <FontAwesomeIcon icon={faMoon} />
+        )}
       </button>
     </footer>
   )

156124f06be6f6494e4ce7a876040cbc0dc87db0
Leo Wong <leoelicos@gmail.com>
2023-02-11 23:58:39 +1100

    Implement green with loading state

--- src/components/12__library-search-tool/index.jsx
+++ src/components/12__library-search-tool/index.jsx
@@ -1,17 +1,16 @@
-import { Button, Empty, Form, Layout, Select, Space } from 'antd'
-import { useForm } from 'antd/es/form/Form'
+import { Button, Divider, Empty, Form, Layout, Select, Spin, Tag } from 'antd'
 import Input from 'antd/es/input/Input'
 import { Content, Footer, Header } from 'antd/es/layout/layout'
 import Typography from 'antd/es/typography/Typography'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import useResize from './hooks/useResize'
 const { Text, Title } = Typography
 const thinBar = {
   height: 'min-content',
   lineHeight: 'unset',
   textAlign: 'center',
-  color: '#070707',
-  backgroundColor: '#f0feff',
+  color: '#fff',
+  backgroundColor: '#376BB3',
   padding: 0,
   display: 'flex',
   alignItems: 'center',
@@ -22,66 +21,89 @@ const thinBarText = {
 }
 const useData = () => {
   const [results, setResults] = useState({ results: [], searchText: '', searchFormat: '' })
-  const loading = useRef(false)
+  const [loading, setLoading] = useState(false)
+  const [savedText, setSavedText] = useState('')
+  const [savedFormat, setSavedFormat] = useState('')
+
   const queryResults = (text, format) => {
-    console.log('here')
-    loading.current = true
-    fetch(format === undefined ? `https://loc.gov/search/?q=${text}&fo=json` : `https://loc.gov/${format}/?q=${text}&fo=json`)
+    setSavedText('')
+    setSavedFormat('')
+    setLoading(true)
+
+    fetch(
+      format === undefined //
+        ? `https://loc.gov/search/?q=${text}&fo=json`
+        : `https://loc.gov/${format}/?q=${text}&fo=json`
+    )
       .then((response) => response.json())
       .then((data) => {
-        console.log({ data })
-        loading.current = false
-        const obj = {
-          results: data.results.map((d) => ({
-            date: d.date || '',
-            title: d.title || '',
-            url: d.url || '',
-            subject: d.subject || '',
-            description: d.description || ''
-          })),
-          searchText: text,
-          searchFormat: format
-        }
-        console.log('results object', obj)
-        setResults(obj)
+        setResults(
+          data.results
+            .map((d) => ({
+              date: d.date || '',
+              title: d.title || '',
+              url: d.url || '',
+              subject: d.subject || '',
+              description: d.description || ''
+            }))
+            .slice(0, 2)
+        )
+        setSavedText(text)
+        setSavedFormat(format)
+
+        setLoading(false)
       })
       .catch((e) => {
         console.error(e)
       })
   }
 
-  return { results, queryResults, loading: loading.current }
+  return { results, queryResults, loading, savedText, savedFormat }
 }
 function LibrarySearchTool() {
   /* state */
   const [resultPage, setResultPage] = useState(false)
   const [form] = Form.useForm()
   const width = useResize()
-  const { results, queryResults, loading } = useData()
-  const handleSubmit = (e) => {
-    console.log('submitted', e['search-text'], e['search-format'])
-    if (!resultPage) setResultPage(true)
+  const { results, queryResults, loading, savedText, savedFormat } = useData()
 
+  /* focus on mount */
+  const searchElement = useRef(null)
+  useEffect(() => {
+    searchElement.current.focus()
+  }, [])
+
+  const handleSubmit = (e) => {
+    if (!resultPage) setResultPage(true)
     form.resetFields()
     queryResults(e['search-text'], e['search-format'])
   }
+
   return (
-    <Layout style={{ height: '100vh', width: '100%', display: 'flex', flexDirection: 'column' }}>
+    <Layout
+      style={{
+        height: '100vh',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
       <Header style={thinBar}>
-        <Text style={thinBarText}>{resultPage ? 'Results' : 'Search'}</Text>
+        <Text style={thinBarText}>{resultPage ? 'Results' : 'Catalog'}</Text>
       </Header>
       <Layout
-        style={{
-          display: 'flex',
-          flexDirection: resultPage && width > 600 ? 'row' : 'column',
-          justifyContent: resultPage ? 'start' : 'center',
-          alignItems: resultPage ? 'stretch' : 'center'
-        }}>
+        style={
+          resultPage //
+            ? width < 600
+              ? { display: 'block', backgroundColor: 'black', height: '100%' }
+              : { flexDirection: 'row' }
+            : null
+        }>
         <Content /* form container */
           style={
             !resultPage
               ? {
-                  background: 'hotpink',
+                  background: '#FFDD9C',
+
                   display: 'flex',
                   flexDirection: 'column',
                   justifyContent: 'center',
@@ -91,35 +113,34 @@ function LibrarySearchTool() {
                 }
               : width < 600
               ? {
-                  background: 'red',
-                  border: '1px solid green !important',
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'start',
-                  alignItems: 'stretch',
-                  width: '100%',
-                  height: 'fit-content'
+                  background: '#FFDD9C'
                 }
               : {
-                  background: 'yellow',
+                  background: '#FFDD9C',
                   display: 'flex',
-                  flexDirection: 'row',
                   justifyContent: 'start',
                   alignItems: 'stretch',
-                  width: '100%',
-                  height: 'fit-content'
+                  width: '33%',
+                  height: '100%',
+                  padding: '0 1rem'
                 }
           }>
-          <Title
-            level={2}
-            style={{ margin: '0', maxWidth: '400px' }}>
-            Library of&nbsp;Congress Search&nbsp;Engine
-          </Title>
+          {!resultPage && (
+            <Title
+              level={2}
+              style={{ margin: '0 0 1rem', maxWidth: '400px' }}>
+              Library of Congress
+              <Divider />
+              Catalog
+            </Title>
+          )}
           <Form
+            size='large'
+            layout='horizontal'
             form={form}
             name='search'
             onFinish={handleSubmit}
-            style={{ width: '100%', maxWidth: '400px' }}>
+            style={{ width: '100%', maxWidth: '600px', padding: '1rem' }}>
             <Form.Item
               name='search-text'
               rules={[
@@ -128,9 +149,14 @@ function LibrarySearchTool() {
                   message: 'Please enter something to search'
                 }
               ]}>
-              <Input placeholder='Search' />
+              <Input
+                placeholder='Search'
+                ref={searchElement}
+              />
             </Form.Item>
-            <Form.Item name='search-format'>
+            <Form.Item
+              name='search-format'
+              style={{ marginBottom: '2px !important' }}>
               <Select
                 allowClear
                 id='search-format'
@@ -148,7 +174,7 @@ function LibrarySearchTool() {
                 style={{ textAlign: 'left' }}
               />
             </Form.Item>
-            <Form.Item>
+            <Form.Item style={{ marginBottom: '2px !important' }}>
               <Button
                 htmlType='submit'
                 type='primary'
@@ -160,7 +186,7 @@ function LibrarySearchTool() {
             {resultPage && (
               <Form.Item>
                 <Button
-                  type='ghost'
+                  type='default'
                   block
                   onClick={() => {
                     setResultPage(false)
@@ -172,35 +198,113 @@ function LibrarySearchTool() {
           </Form>
         </Content>
         {resultPage ? (
-          <Content style={{ backgroundColor: loading ? 'red' : 'green', width: width < 600 ? '100%' : '50%', overflowY: 'auto', height: '100%' }}>
-            {loading ? (
-              'Loading'
-            ) : (
-              <>
-                <h1>
-                  Showing{results?.searchFormat?.length > 0 ? ` ${results.searchFormat}` : ''} results for {results.searchText}
-                </h1>
-                <ul>
-                  {(results?.results?.length &&
-                    results.results.map((result) => (
-                      <li>
-                        <h2>{result.title}</h2>
-                        <ul>
-                          <li>{result.url}</li>
-                          <li>{result.date}</li>
-                          <li>{result.subject}</li>
-                          <li>{result.description}</li>
+          <Content
+            style={
+              width < 600
+                ? {
+                    textAlign: 'center',
+                    backgroundColor: loading ? 'black' : '#B38836'
+                  }
+                : {
+                    width: '66%',
+                    overflowY: 'auto',
+                    height: '100%',
+                    padding: '0 1rem',
+
+                    textAlign: 'center',
+                    backgroundColor: loading ? 'black' : '#B38836'
+                  }
+            }>
+            <>
+              <Title
+                level={5}
+                style={{ marginTop: '1rem', color: 'white' }}>
+                {`${loading ? 'Loading' : 'Showing'} ${savedFormat?.length > 0 ? ` ${savedFormat.toLowerCase()}` : ''} results for ${savedText}`}
+              </Title>
+              {loading ? (
+                <Spin style={{ marginTop: '1rem' }} />
+              ) : (
+                <ul
+                  style={{
+                    listStyleType: 'none',
+                    margin: '0',
+                    padding: '0'
+                  }}>
+                  {(results?.length &&
+                    results?.map((result) => (
+                      <li
+                        key={result.description}
+                        style={{
+                          backgroundColor: '#ffffff',
+                          margin: '0.5rem 0',
+                          padding: '0.5rem'
+                        }}>
+                        <Title
+                          level={4}
+                          style={{
+                            marginTop: '0'
+                          }}>
+                          {result.title}
+                        </Title>
+                        <ul
+                          style={{
+                            listStyleType: 'none',
+                            margin: '0',
+                            padding: '0'
+                          }}>
+                          {result.date && (
+                            <li
+                              style={{
+                                margin: '0.5rem 0'
+                              }}>
+                              <Text strong>Date: </Text>
+                              {result.date}
+                            </li>
+                          )}
+                          {result.subject && (
+                            <li style={{ margin: '0.5rem 0' }}>
+                              <Text strong>
+                                Keywords:&nbsp;
+                                {result.subject.map((s) => (
+                                  <Tag
+                                    key={s}
+                                    color='blue'>
+                                    {s}
+                                  </Tag>
+                                ))}
+                              </Text>
+                            </li>
+                          )}
+                          {result.description && (
+                            <li style={{ margin: '0.5rem 0' }}>
+                              <Text strong>Description: </Text>
+                              {result.description}
+                            </li>
+                          )}
+                          {result.url && (
+                            <li style={{ marginTop: '0.5rem' }}>
+                              <Button
+                                size='small'
+                                block
+                                type='default'
+                                onClick={() => {
+                                  window.open(result.url, '_blank')
+                                }}>
+                                Read more
+                              </Button>
+                            </li>
+                          )}
                         </ul>
                       </li>
                     ))) || <Empty />}
                 </ul>
-              </>
-            )}
+              )}
+            </>
           </Content>
         ) : null}
       </Layout>
       <Footer style={thinBar}>
-        <Text style={thinBarText}>Library Search Tool</Text>
+        <Text style={thinBarText}>Library of Congress</Text>
       </Footer>
     </Layout>
   )

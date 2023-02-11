40860875039c9751c579df944c1eca1599195558
Leo Wong <leoelicos@gmail.com>
2023-02-12 00:01:55 +1100

    Starter code

--- src/hooks/useFavicon.js
+++ src/hooks/useFavicon.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 const useFavicon = (src) => {
   useEffect(() => {
-    document.querySelector('#favicon').setAttribute('href', 'favicons/11-favicon.png')
+    document.querySelector('#favicon').setAttribute('href', src)
     return () => {
       document.querySelector('#favicon').setAttribute('href', src)
     }

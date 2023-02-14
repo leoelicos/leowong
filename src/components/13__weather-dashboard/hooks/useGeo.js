a44b428ac66d6b500c63ba7509c19305860e70bc
Leo Wong <leoelicos@gmail.com>
2023-02-15 03:36:57 +1100

    Filter cities to be unique

--- src/components/13__weather-dashboard/hooks/useGeo.js
+++ src/components/13__weather-dashboard/hooks/useGeo.js
@@ -5,16 +5,17 @@ const useGeo = (updateCityOptions) => {
     try {
       const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${REACT_APP_13_OPENWEATHER_APPID}&units=metric&lang=en`)
       const data = await response.json()
+      const serialized = data.map((city) => ({
+        lat: city.lat,
+        lon: city.lon,
+        city: city.name,
+        country: city.country,
+        state: city.state
+      }))
+      const fingerprint = serialized.map(({ city, country, state }) => `${city}_${country}_${state}`)
+      const unique = serialized.filter(({ city, country, state }, i) => fingerprint.indexOf(`${city}_${country}_${state}`) === i)
 
-      updateCityOptions(
-        data.map((city) => ({
-          lat: city.lat,
-          lon: city.lon,
-          city: city.name,
-          country: city.country,
-          state: city.state
-        }))
-      )
+      updateCityOptions(unique)
     } catch (error) {
       console.error(error)
     }

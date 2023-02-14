581c246e76e6db3864c6f124081037e072288abd
Leo Wong <leoelicos@gmail.com>
2023-02-15 02:39:42 +1100

    Handle all Header logic here

--- src/components/13__weather-dashboard/components/Header/index.jsx
+++ src/components/13__weather-dashboard/components/Header/index.jsx
@@ -1,33 +1,66 @@
+import { useEffect, useRef, useState } from 'react'
+
+/* Custom components */
 import Globe from './Globe'
-/* components */
-import { useRef, useState } from 'react'
-import InputCity from './components/InputCity.jsx'
-import SelectCity from './components/SelectCity.jsx'
-import SelectHistoryCity from './components/SelectHistoryCity.jsx'
+
+/* antd components */
+import { Select } from 'antd'
 
 /* style */
 import './style/header.css'
-function Header({ savedCities, saveCity }) {
-  /* state */
-  const [searchValue, setSearchValue] = useState('')
-  const selectCityRef = useRef(null)
-  const [selectCityOpen, setSelectCityOpen] = useState(false)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
-  /* event handlers */
-  const search = (city) => {
-    console.log('Now searching for', city)
-  }
+function Header({ savedCities, saveCity, handleClickHistory, searchCoords, cityOptions, searchCities }) {
+  /* state */
+  const [active, setActive] = useState(0)
+  const [selectCityOpen, setSelectCityOpen] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+
+  /* DOM refs */
+  const inputRef = useRef(null)
+  const selectCityRef = useRef(null)
+  const selectHistoryRef = useRef(null)
+
+  /* input */
   const handleSearchValueChange = (val) => {
     setSearchValue(val)
   }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log('clicked submit', searchValue)
-    // gray the InputCity
-    // focus SelectCity
-    selectCityRef.current.setAttribute('size', '5')
+  const handleSubmit = () => {
+    if (!searchValue.length) return
+    setActive(1)
+    searchCities(searchValue)
+    setSelectCityOpen(true)
     selectCityRef.current.focus()
   }
+
+  /* button2 */
+  const handleSelectCity = (optionIdx) => {
+    /* close select element */
+    setSelectCityOpen(false)
+
+    /* get city with selected optionIdx */
+    let selectedCity = cityOptions[optionIdx]
+
+    /* update local storage */
+    saveCity(selectedCity)
+
+    /* search for the city */
+    searchCoords(selectedCity.lat, selectedCity.lon)
+  }
+
+  /* button3 */
+  const handleSelectCityHistory = (e) => {
+    handleClickHistory(e)
+  }
+
+  /* focus logic */
+  useEffect(() => {
+    if (active === 2) selectHistoryRef.current.focus()
+    else if (active === 1) selectCityRef.current.focus()
+    else inputRef.current.focus()
+  }, [active])
+
   return (
     <header className='header'>
       <Globe />
@@ -37,23 +70,56 @@ function Header({ savedCities, saveCity }) {
         <form
           className='buttons'
           onSubmit={handleSubmit}>
-          <InputCity
-            searchValue={searchValue}
-            handleSearchValueChange={handleSearchValueChange}
-            onClickSearchIcon={handleSubmit}
-            isActive={active === 0}
-          />
-          <SelectCity
+          <div className='search-container'>
+            <input
+              ref={inputRef}
+              className={`form-select ${active === 0 ? '' : 'gray'}`}
+              type='search'
+              id='search'
+              list='citylist'
+              placeholder='CITY'
+              value={searchValue}
+              onChange={(e) => {
+                handleSearchValueChange(e.target.value)
+              }}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') handleSubmit()
+              }}
+            />
+            <button
+              type='button'
+              id='searchBtn'
+              className='button'
+              onClick={(e) => {
+                e.preventDefault()
+                handleSubmit()
+              }}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+          <Select
+            className={`form-select ${active === 1 ? '' : 'gray'}`}
             ref={selectCityRef}
-            saveCity={saveCity}
+            onChange={handleSelectCity}
+            onDropdownVisibleChange={(visible) => {
+              setSelectCityOpen(visible)
+            }}
             open={selectCityOpen}
-            isActive={active === 1}
+            options={cityOptions.map(({ country, state, city }, i) => ({ key: `${country}_${state}_${city}`, value: i, label: `${city} (${state}, ${country})` }))}
           />
-          <SelectHistoryCity
-            savedCities={savedCities}
-            search={search}
-            isActive={active === 2}
-          />
+
+          <Select
+            className={`form-select ${active === 2 ? '' : 'gray'}`}
+            onChange={handleSelectCityHistory}
+            ref={selectHistoryRef}>
+            {savedCities?.map(({ country, state, city, lat, lon }, i) => (
+              <Select.Option
+                key={i}
+                value={`${lat}_${lon}`}>
+                {`${city} (${state}, ${country})`}
+              </Select.Option>
+            )) || null}
+          </Select>
         </form>
       </div>
     </header>

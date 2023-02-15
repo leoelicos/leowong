6659f2b0ce7088e3dbee7f060cd6612dc433c19b
Leo Wong <leoelicos@gmail.com>
2023-02-16 02:48:23 +1100

    Remove undefined return value

--- src/components/14__moovee/api/youtubeSearchAPI.js
+++ src/components/14__moovee/api/youtubeSearchAPI.js
@@ -4,10 +4,10 @@ const { REACT_APP_14_GAPI_KEY } = process.env
 
 const searchYouTube = (term) =>
   new Promise((res) => {
-    const x = YTSearch({ key: REACT_APP_14_GAPI_KEY, term: term }, (videos) => {
+    res('https://www.youtube.com/embed/8qi_EO6jQvM')
+    /* YTSearch({ key: REACT_APP_14_GAPI_KEY, term: term }, (videos) => {
       res(`https://www.youtube.com/embed/${videos[0].id.videoId}`)
-    })
-    console.log({ x })
+    }) */
   })
 
 export default searchYouTube

6e21d8d3be37db4ed1bb54bb7f21df8f694dc723
Leo Wong <leoelicos@gmail.com>
2023-02-17 22:55:39 +1100

    Delete testing code

--- src/components/14__moovee/api/youtubeSearchAPI.js
+++ src/components/14__moovee/api/youtubeSearchAPI.js
@@ -1,16 +1,12 @@
-// import YTSearch from 'youtube-api-search'
+import YTSearch from 'youtube-api-search'
 
-// const { REACT_APP_14_GAPI_KEY } = process.env
+const { REACT_APP_14_GAPI_KEY } = process.env
 
 const searchYouTube = (term) =>
   new Promise((res) => {
-    setTimeout(() => {
-      res('https://www.youtube.com/embed/tPEE9ZwTmy0')
-
-      // YTSearch({ key: REACT_APP_14_GAPI_KEY, term: term }, (videos) => {
-      //   res(`https://www.youtube.com/embed/${videos[0].id.videoId}`)
-      // })
-    }, 1000)
+    YTSearch({ key: REACT_APP_14_GAPI_KEY, term: term }, (videos) => {
+      res(`https://www.youtube.com/embed/${videos[0].id.videoId}`)
+    })
   })
 
 export default searchYouTube

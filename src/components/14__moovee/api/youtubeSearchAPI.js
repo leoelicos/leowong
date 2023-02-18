aa5774b4586aae5793767cbfc612648221f65770
Leo Wong <leoelicos@gmail.com>
2023-02-18 19:56:26 +1100

    replace youtube-api-search with axios

--- src/components/14__moovee/api/youtubeSearchAPI.js
+++ src/components/14__moovee/api/youtubeSearchAPI.js
@@ -1,12 +1,28 @@
-import YTSearch from 'youtube-api-search'
+var axios = require('axios')
 
 const { REACT_APP_14_GAPI_KEY } = process.env
+var ROOT_URL = 'https://www.googleapis.com/youtube/v3/search'
 
-const searchYouTube = (term) =>
-  new Promise((res) => {
-    YTSearch({ key: REACT_APP_14_GAPI_KEY, term: term }, (videos) => {
-      res(`https://www.youtube.com/embed/${videos[0].id.videoId}`)
-    })
-  })
+const searchYouTube = async (term) => {
+  if (!REACT_APP_14_GAPI_KEY) throw new Error('Gapi key undefined')
+
+  let firstVideoId = ''
+  try {
+    var params = {
+      part: 'snippet',
+      key: REACT_APP_14_GAPI_KEY,
+      q: term,
+      type: 'video'
+    }
+    const response = await axios.get(ROOT_URL, { params })
+    const videos = response?.data?.items || null
+    firstVideoId = (videos && videos[0]?.id?.videoId) || ''
+    if (firstVideoId === '') throw new Error('No video found')
+  } catch (e) {
+    console.error(e)
+  } finally {
+    return firstVideoId
+  }
+}
 
 export default searchYouTube

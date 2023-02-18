903497671dd54e990cb406f6437dbbdde1b4573e
Leo Wong <leoelicos@gmail.com>
2023-02-18 19:57:29 +1100

    Extract MooHeader and Card (MooCard) external file

--- src/components/14__moovee/index.jsx
+++ src/components/14__moovee/index.jsx
@@ -10,73 +10,43 @@ import useFavicon from '../../hooks/useFavicon'
 import searchYouTube from './api/youtubeSearchAPI'
 
 /* components */
-import { Affix, Button, ConfigProvider, Progress, Select, Space, Spin, Tag, Tooltip } from 'antd'
-import Search from 'antd/es/input/Search'
-import Title from 'antd/es/typography/Title'
+import { Button, ConfigProvider, Progress, Space, Tag, Tooltip } from 'antd'
 
-/* images */
-import Logo from './images/moovee.png'
-import SmallLogo from './images/mooveefavi.png'
-import NoPoster from './images/noposter.png'
+import MooHeader from './components/MooHeader'
+import MooResults from './components/MooResults'
+
+/* icons */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretSquareDown } from '@fortawesome/free-regular-svg-icons'
-import { faClockRotateLeft, faFaceMehBlank, faMessage } from '@fortawesome/free-solid-svg-icons'
+import { faMessage } from '@fortawesome/free-solid-svg-icons'
 
 /* style */
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 import './css/index.css'
 
-const MooHeader = ({ searched, loading, handleSubmit, savedMovies, handleSelectMovieHistory }) => (
-  <Affix
-    offsetTop={0}
-    className='affix'>
-    <header className={searched ? 'header' : 'header unsearched'}>
-      <img
-        src={searched ? SmallLogo : Logo}
-        alt='moovee logo'
-      />
-      <div className='header-elements'>
-        <Search
-          loading={loading}
-          allowClear={true}
-          className='movie-search'
-          placeholder={searched ? '…' : 'Any movie name…'}
-          enterButton={searched ? 'Find' : 'Find Trailer'}
-          onSearch={handleSubmit}
-        />
-        <Select
-          className='history-select'
-          disabled={savedMovies.length === 0}
-          dropdownMatchSelectWidth={false}
-          bordered={false}
-          onChange={handleSelectMovieHistory}
-          suffixIcon={<FontAwesomeIcon icon={faCaretSquareDown} />}
-          placeholder={<FontAwesomeIcon icon={faClockRotateLeft} />}
-          options={savedMovies?.map((title, i) => ({
-            value: i,
-            label: title
-          }))}
-        />
-      </div>
-    </header>
-  </Affix>
-)
-
 export default function MooVee() {
-  /* track OMDB promise */
-  const loadingOMDB = useRef(false)
+  /* favicon */
+  useFavicon('/favicons/14-favicon.png')
 
-  /* track user */
+  /* title */
+  useTitle('MooVee')
+
+  /* results will mount */
   let hasSearched = useRef(false)
 
   /* local storage */
   const { savedMovies, saveMovie } = useLocalStorage('titles')
-  useFavicon('/favicons/14-favicon.png')
-  useTitle('MooVee')
-  const { searchOMDB, OMDBmovies } = useOMDB()
 
-  /* event handlers */
+  /* OMDB */
+  const { searchOMDB, OMDBMovies } = useOMDB()
+
+  /* OMDB loading */
+  const loadingOMDB = useRef(false)
+
+  /* modal with trailer */
+  const { Trailer, showTrailer, updateUri, updateLoadingGapi } = useTrailer()
+
+  /* handle search */
   const handleSubmit = async (e) => {
+    console.log('clicked submit', e)
     let m = e.replace(/[^a-zA-Z0-9 ]/g, '').trim()
     if (!m.length) return
 
@@ -84,20 +54,24 @@ export default function MooVee() {
 
     const c = m[0].toLocaleUpperCase() + m.slice(1).toLocaleLowerCase()
     saveMovie(c) /* save to local storage */
-
+    console.log('saved to local storage')
     loadingOMDB.current = true
     await searchOMDB(c)
+    console.log('finished saving to local storage')
+    console.log({ OMDBMovies })
     loadingOMDB.current = false
   }
-  const { Trailer, showTrailer, updateUri, updateLoadingGapi } = useTrailer()
+
+  /* handle trailer */
   const handleClickTrailer = async (title, year) => {
     showTrailer()
     let gapiRes = ''
     try {
       updateLoadingGapi(true)
+      console.log('searching youtube')
       gapiRes = await searchYouTube(`${title}+${year}+trailer`)
+      console.log({ gapiRes })
       updateLoadingGapi(false)
-      // console.log({ gapiRes })
     } catch (error) {
       console.error(error)
     } finally {
@@ -105,11 +79,10 @@ export default function MooVee() {
       updateUri(gapiRes)
     }
   }
+
+  /* handle click history option */
   const handleSelectMovieHistory = (historyIdx) => {
-    handleClickHistory(savedMovies[historyIdx])
-  }
-  const handleClickHistory = (historyMovie) => {
-    handleSubmit(historyMovie)
+    handleSubmit(savedMovies[historyIdx])
   }
 
   return (
@@ -122,55 +95,19 @@ export default function MooVee() {
             }
           }}>
           <MooHeader
-            searched={hasSearched.current}
-            loading={loadingOMDB.current}
+            searched={hasSearched?.current}
+            loading={loadingOMDB?.current}
             handleSubmit={handleSubmit}
             savedMovies={savedMovies}
             handleSelectMovieHistory={handleSelectMovieHistory}
           />
+          <MooResults
+            searched={hasSearched?.current}
+            loading={loadingOMDB?.current}
+            OMDBMovies={OMDBMovies}
+            handleClickTrailer={handleClickTrailer}
+          />
 
-          {!hasSearched?.current ? null : loadingOMDB.current ? (
-            <main>
-              <Spin
-                size='large'
-                className='loading-spin'
-              />
-            </main>
-          ) : !OMDBmovies || !OMDBmovies.length ? (
-            <main>
-              <div className='result-container empty'>
-                <Title
-                  className='no-movies'
-                  level={5}>
-                  N
-                  <FontAwesomeIcon icon={faFaceMehBlank} />
-                  &ensp;m
-                  <FontAwesomeIcon icon={faFaceMehBlank} />
-                  vies&ensp;f
-                  <FontAwesomeIcon icon={faFaceMehBlank} />
-                  und
-                </Title>
-              </div>
-            </main>
-          ) : (
-            <main>
-              <ResponsiveMasonry
-                columnsCountBreakPoints={{ 0: 1, 450: 2, 679: 3 }}
-                style={{}}>
-                <Masonry
-                  style={{ gap: '4px', maxWidth: 'calc(900px + 8px)' }}
-                  className='movie-grid'>
-                  {OMDBmovies.map((movie, i) => (
-                    <Card
-                      key={i}
-                      {...movie}
-                      handleClickTrailer={handleClickTrailer}
-                    />
-                  ))}
-                </Masonry>
-              </ResponsiveMasonry>
-            </main>
-          )}
           <footer>
             <Button
               type='text'
@@ -185,93 +122,6 @@ export default function MooVee() {
           </footer>
           <Trailer />
         </ConfigProvider>
-      </div>
-    </div>
-  )
-}
-
-const Card = ({ poster, title, esrb, year, genre, actors, plot, rating, handleClickTrailer }) => {
-  return (
-    <div className='card'>
-      <div
-        className='result-container'
-        key={poster}>
-        <div className='movie-poster'>
-          <img
-            src={poster === 'N/A' ? NoPoster : poster}
-            alt='movie poster'
-          />
-        </div>
-
-        <div className='movie-details'>
-          <h3 className='title'>{title === 'N/A' ? 'Untitled' : title} </h3>
-
-          <Button
-            className='trailer-btn'
-            type='primary'
-            onClick={() => {
-              handleClickTrailer(title, year)
-            }}>
-            Watch Trailer
-          </Button>
-          <div className='plot'>{plot === 'N/A' ? '—' : plot}</div>
-          <div className='rating'>
-            {rating.length > 0 && (
-              <Tooltip
-                title={rating[0].Source}
-                placement='bottom'>
-                <div
-                  style={{
-                    backgroundColor: 'rgba(0,0,0,0.5)',
-                    borderRadius: '1rem',
-                    padding: '0 0 1px 8px '
-                  }}>
-                  <Progress
-                    percent={parseFloat(rating[0].Value) * 10}
-                    format={(percent) => parseFloat(percent / 10).toFixed(1)}
-                    style={{ color: 'white' }}
-                    steps={10}
-                    status={parseInt(rating[0].Value) < 7 ? 'exception' : 'success'}
-                    strokeColor={parseInt(rating[0].Value) < 4 ? 'red' : parseInt(rating[0].Value) < 7 ? 'orange' : 'green'}
-                  />
-                </div>
-              </Tooltip>
-            )}
-          </div>
-          <div>
-            {year === 'N/A' ? '—' : year}
-            <br />
-            {esrb === 'N/A' ? 'Unrated' : esrb}
-          </div>
-          <div className='actors'>
-            {actors === 'N/A'
-              ? '—'
-              : actors.split(', ').map((actor, i) => (
-                  <Tag
-                    key={i}
-                    color='#222'
-                    style={{ color: 'white' }}>
-                    {actor}
-                  </Tag>
-                ))}
-          </div>
-          {genre === 'N/A' ? null : (
-            <div className='genre'>
-              <Space
-                style={{ justifyContent: 'center' }}
-                size={[0, 8]}
-                wrap>
-                {genre.split(', ').map((g, i) => (
-                  <Tag
-                    color='black'
-                    key={i}>
-                    {g}
-                  </Tag>
-                ))}
-              </Space>
-            </div>
-          )}
-        </div>
       </div>
     </div>
   )

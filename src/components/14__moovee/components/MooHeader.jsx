489c522d05747738872ac047ab058b6ad13c0736
Leo Wong <leoelicos@gmail.com>
2023-02-20 04:57:59 +1100

    Add wrapper .header-contents to fix box shadow not 100%

--- src/components/14__moovee/components/MooHeader.jsx
+++ src/components/14__moovee/components/MooHeader.jsx
@@ -13,37 +13,39 @@ const MooHeader = ({ searched, loading, handleSubmit, history, handleSelectMovie
     offsetTop={0}
     className='affix'>
     <header className={!searched ? 'unsearched' : 'none'}>
-      <img
-        src={searched ? SmallLogo : Logo}
-        alt='moovee logo'
-      />
-      {!searched && (
-        <p>
-          <em>Tonight, I'll be watching…</em>
-        </p>
-      )}
-      <div className='header__elements'>
-        <Search
-          loading={loading}
-          allowClear={true}
-          className='movie-search'
-          placeholder={searched ? '…' : 'Any movie name…'}
-          enterButton={<FontAwesomeIcon icon={faSearch} />}
-          onSearch={handleSubmit}
+      <div className='header-contents'>
+        <img
+          src={searched ? SmallLogo : Logo}
+          alt='moovee logo'
         />
-        <Select
-          notFoundContent={<Empty />}
-          className='history-select'
-          dropdownMatchSelectWidth={false}
-          bordered={false}
-          onChange={handleSelectMovieHistory}
-          suffixIcon={<FontAwesomeIcon icon={faCaretSquareDown} />}
-          placeholder='Search history'
-          options={history?.map((title, i) => ({
-            value: i,
-            label: title
-          }))}
-        />
+        {!searched && (
+          <p>
+            <em>Tonight, I'll be watching…</em>
+          </p>
+        )}
+        <div className='header__elements'>
+          <Search
+            loading={loading}
+            allowClear={true}
+            className='movie-search'
+            placeholder={searched ? '…' : 'Any movie name…'}
+            enterButton={<FontAwesomeIcon icon={faSearch} />}
+            onSearch={handleSubmit}
+          />
+          <Select
+            notFoundContent={<Empty />}
+            className='history-select'
+            dropdownMatchSelectWidth={false}
+            bordered={false}
+            onChange={handleSelectMovieHistory}
+            suffixIcon={<FontAwesomeIcon icon={faCaretSquareDown} />}
+            placeholder='Search history'
+            options={history?.map((title, i) => ({
+              value: i,
+              label: title
+            }))}
+          />
+        </div>
       </div>
     </header>
   </Affix>

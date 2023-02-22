3a96de9100ad7c51bd0aa8667c9d1859be733d35
Leo Wong <leoelicos@gmail.com>
2023-02-22 22:41:04 +1100

    import Markdown to render content

--- src/components/15__readme-maker/index.jsx
+++ src/components/15__readme-maker/index.jsx
@@ -1,6 +1,9 @@
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './css/index.css'
+import Markdown from 'marked-react'
+
+import types from './data/types'
 
 const Header = ({ children }) => {
   return <header>{children}</header>
@@ -18,16 +21,24 @@ const ReadmeMaker = () => {
   return (
     <div className='app-15'>
       <div className='body'>
-        <Header>Readme Maker</Header>
+        <Header>
+          <h1>Readme Maker</h1>
+        </Header>
         <Content>
-          {new Array(4).fill(Math.random(100)).map((v, i) => (
-            <article key={i}>{v}</article>
+          {types.map(({ name, content }) => (
+            <article key={name}>
+              <div>
+                <Markdown>{content}</Markdown>
+              </div>
+            </article>
           ))}
         </Content>
         <Footer>
-          Made with&ensp;
-          <FontAwesomeIcon icon={faHeart} />
-          &ensp;and React
+          <h2>
+            Made with&ensp;
+            <FontAwesomeIcon icon={faHeart} />
+            &ensp;and React
+          </h2>
         </Footer>
       </div>
     </div>

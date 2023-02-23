e7d2bdb46dd1f607980b07994fc36c2f4895aa78
Leo Wong <leoelicos@gmail.com>
2023-02-24 05:23:09 +1100

    Implement antd's notification on successful generate

--- src/components/15__readme-maker/index.jsx
+++ src/components/15__readme-maker/index.jsx
@@ -1,12 +1,10 @@
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './css/index.css'
 import Markdown from 'marked-react'
 
 import data from './data/types'
-import { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
-import { Tooltip } from 'antd'
+import { Button, notification, Tooltip } from 'antd'
 
 const Header = ({ children }) => {
   return <header>{children}</header>
@@ -19,15 +17,27 @@ const Content = ({ children }) => {
 const Footer = ({ children }) => {
   return <footer>{children}</footer>
 }
-
+const Context = React.createContext({
+  name: 'Default'
+})
 const ReadmeMaker = () => {
   const initialTypes = data.map((t) => ({ ...t, key: t.name, selected: false }))
 
   const [types, setTypes] = useState(initialTypes)
   const handleClick = ({ name }) => {
-    console.log({ name })
     setTypes((prev) => prev.map((type) => (type.name === name ? { ...type, selected: !type.selected } : type)))
   }
+  const [api, contextHolder] = notification.useNotification()
+  const openNotification = () => {
+    api.info({
+      //
+      message: 'Success!',
+      description: <Context.Consumer>{({ name }) => `${name}`}</Context.Consumer>,
+      placement: 'bottomRight',
+      duration: 1
+    })
+  }
+  const contextValue = useMemo(() => ({ name: 'Markdown copied to clipboard' }), [])
   return (
     <div className='app-15'>
       <div className='body'>
@@ -40,14 +50,17 @@ const ReadmeMaker = () => {
             {types
               .filter((t) => t.section === 'header')
               .map(({ name, selected, content, key, description }) => (
-                <article
+                <Tooltip
                   key={key}
-                  onClick={() => {
-                    handleClick({ name })
-                  }}
-                  className={selected ? 'selected' : ''}>
-                  <Tooltip title={description}>{<Markdown>{content}</Markdown>}</Tooltip>
-                </article>
+                  title={description}>
+                  <article
+                    onClick={() => {
+                      handleClick({ name })
+                    }}
+                    className={selected ? 'selected' : ''}>
+                    {<Markdown>{content}</Markdown>}
+                  </article>
+                </Tooltip>
               ))}
           </section>
           <h1>Overview</h1>
@@ -55,29 +68,35 @@ const ReadmeMaker = () => {
             {types
               .filter((t) => t.section === 'overview')
               .map(({ name, selected, content, key, description }) => (
-                <article
+                <Tooltip
                   key={key}
-                  onClick={() => {
-                    handleClick({ name })
-                  }}
-                  className={selected ? 'selected' : ''}>
-                  <Tooltip title={description}>{<Markdown>{content}</Markdown>}</Tooltip>
-                </article>
+                  title={description}>
+                  <article
+                    onClick={() => {
+                      handleClick({ name })
+                    }}
+                    className={selected ? 'selected' : ''}>
+                    {<Markdown>{content}</Markdown>}
+                  </article>
+                </Tooltip>
               ))}
           </section>
           <h1>Setup</h1>
           <section className='readme-header'>
             {types
-              .filter((t) => t.section === 'low-level detail')
+              .filter((t) => t.section === 'setup')
               .map(({ name, selected, content, key, description }) => (
-                <article
+                <Tooltip
                   key={key}
-                  onClick={() => {
-                    handleClick({ name })
-                  }}
-                  className={selected ? 'selected' : ''}>
-                  <Tooltip title={description}>{<Markdown>{content}</Markdown>}</Tooltip>
-                </article>
+                  title={description}>
+                  <article
+                    onClick={() => {
+                      handleClick({ name })
+                    }}
+                    className={selected ? 'selected' : ''}>
+                    {<Markdown>{content}</Markdown>}
+                  </article>
+                </Tooltip>
               ))}
           </section>
           <h1>References</h1>
@@ -85,14 +104,17 @@ const ReadmeMaker = () => {
             {types
               .filter((t) => t.section === 'references')
               .map(({ name, selected, content, key, description }) => (
-                <article
+                <Tooltip
                   key={key}
-                  onClick={() => {
-                    handleClick({ name })
-                  }}
-                  className={selected ? 'selected' : ''}>
-                  <Tooltip title={description}>{<Markdown>{content}</Markdown>}</Tooltip>
-                </article>
+                  title={description}>
+                  <article
+                    onClick={() => {
+                      handleClick({ name })
+                    }}
+                    className={selected ? 'selected' : ''}>
+                    {<Markdown>{content}</Markdown>}
+                  </article>
+                </Tooltip>
               ))}
           </section>
           <h1>Footer</h1>
@@ -100,23 +122,48 @@ const ReadmeMaker = () => {
             {types
               .filter((t) => t.section === 'footer')
               .map(({ name, selected, content, key, description }) => (
-                <article
+                <Tooltip
                   key={key}
-                  onClick={() => {
-                    handleClick({ name })
-                  }}
-                  className={selected ? 'selected' : ''}>
-                  <Tooltip title={description}>{<Markdown>{content}</Markdown>}</Tooltip>
-                </article>
+                  title={description}>
+                  <article
+                    onClick={() => {
+                      handleClick({ name })
+                    }}
+                    className={selected ? 'selected' : ''}>
+                    {<Markdown>{content}</Markdown>}
+                  </article>
+                </Tooltip>
               ))}
           </section>
         </Content>
         <Footer>
-          <h2>
-            Made with&ensp;
-            <FontAwesomeIcon icon={faHeart} />
-            &ensp;and React
-          </h2>
+          <Context.Provider value={contextValue}>
+            {contextHolder}
+            <Button
+              onClick={async () => {
+                const output = types
+                  .filter((v) => v.selected)
+                  .map((v) => v.content)
+                  .join('\n\n')
+
+                try {
+                  await navigator.clipboard.writeText(output)
+
+                  openNotification()
+                } catch (e) {
+                  console.error(e)
+                }
+              }}>
+              Generate .md
+            </Button>
+            <Button
+              type='danger'
+              onClick={() => {
+                setTypes((prev) => prev.map((type) => ({ ...type, selected: false })))
+              }}>
+              Reset clicks
+            </Button>
+          </Context.Provider>
         </Footer>
       </div>
     </div>

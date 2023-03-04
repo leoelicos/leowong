189592363ff2a4ef10737e3045a5697c77093d47
Leo Wong <leoelicos@gmail.com>
2023-03-05 03:23:43 +1100

    Abstract config to View and NewEmployees components

--- src/components/17__team-maker/components/Main.js
+++ src/components/17__team-maker/components/Main.js
@@ -10,6 +10,44 @@ import { Button, ConfigProvider } from 'antd'
 import useIDB from '../hooks/useIdb'
 import useLocalStorage from '../hooks/useLocalStorage'
 
+const View = ({ children }) => (
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#0014ab',
+        colorBgBase: '#faebd7',
+        colorBorder: '#0014ab',
+        colorBorderSecondary: 'transparent',
+        colorTextDescription: '#7a6345',
+        colorError: '#b22',
+        colorBgElevated: '#faead6',
+
+        colorPrimaryBg: 'transparent'
+      }
+    }}>
+    <section className='view'>{children}</section>
+  </ConfigProvider>
+)
+
+const NewEmployees = ({ children }) => (
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: 'black',
+        colorBgBase: 'white',
+        colorError: '#b22',
+        colorBorderSecondary: 'transparent',
+
+        colorBorder: '#0014ab',
+        colorBgElevated: 'white',
+
+        colorPrimaryBg: 'transparent'
+      }
+    }}>
+    <section className='new-employees'>{children}</section>
+  </ConfigProvider>
+)
+
 const Main = () => {
   const [newEmployees, setNewEmployees] = useState([])
   const { employeesLS, updateEmployeesLS } = useLocalStorage('team-maker-employees')
@@ -32,6 +70,10 @@ const Main = () => {
     )
   }
 
+  const handleAdd = () => {
+    setNewEmployees((prev) => prev.concat({ id: new Date().getTime() }))
+  }
+
   const handleDelete = (id) => {
     dispatchEmployee({ type: 'removedEmployee', action: { id: id } })
     deleteDb(id)
@@ -49,21 +91,20 @@ const Main = () => {
     const initialiseEmployeeContext = async () => {
       try {
         const idb = await getDb()
-
         if (idb?.length > 0) {
           updateEmployeesLS(idb)
           idb.forEach((e) => dispatchEmployee({ type: 'savedEmployee', action: e }))
         } else if (ls?.length > 0) {
           ls.forEach((e) => {
             putDb(e)
+            dispatchEmployee({ type: 'savedEmployee', action: e })
           })
-          ls.forEach((e) => dispatchEmployee({ type: 'savedEmployee', action: e }))
         } else {
           updateEmployeesLS(fallback)
           fallback.forEach((e) => {
             putDb(e)
+            dispatchEmployee({ type: 'savedEmployee', action: e })
           })
-          fallback.forEach((e) => dispatchEmployee({ type: 'savedEmployee', action: e }))
         }
       } catch (e) {
         console.error(e)
@@ -75,82 +116,50 @@ const Main = () => {
   return (
     <main>
       {!employees?.length ? null : (
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: '#0014ab',
-              colorBgBase: '#faebd7',
-              colorBorder: '#0014ab',
-              colorBorderSecondary: 'transparent',
-              colorTextDescription: '#7a6345',
-              colorError: '#b22',
-              colorBgElevated: '#faead6',
-
-              colorPrimaryBg: 'transparent'
-            }
-          }}>
-          <section className='view'>
-            {employees
-              .sort((a, b) => a.role - b.role)
-              .map(({ id, name, email, role }) => (
-                <EmployeeCard
-                  key={id}
-                  id={id}
-                  name={name}
-                  email={email}
-                  role={role}
-                  initialValues={{ id, name, email, role }}
-                  actualValues={{ id, name, email, role }}
-                  onDelete={() => {
-                    handleDelete(id)
-                  }}
-                  onSave={handleSave}
-                />
-              ))}
-          </section>
-        </ConfigProvider>
+        <View>
+          {employees
+            .sort((a, b) => a.role - b.role)
+            .map(({ id, name, email, role }) => (
+              <EmployeeCard
+                key={id}
+                id={id}
+                name={name}
+                email={email}
+                role={role}
+                initialValues={{ id, name, email, role }}
+                actualValues={{ id, name, email, role }}
+                onDelete={() => {
+                  handleDelete(id)
+                }}
+                onSave={handleSave}
+              />
+            ))}
+        </View>
       )}
-      <section className='new-employees'>
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: 'black',
-              colorBgBase: 'white',
-              colorError: '#b22',
-              colorBorderSecondary: 'transparent',
-
-              colorBorder: '#0014ab',
-              colorBgElevated: 'white',
-
-              colorPrimaryBg: 'transparent'
-            }
-          }}>
-          <Button
-            style={{ boxShadow: 'none' }}
-            type='primary'
-            onClick={() => {
-              setNewEmployees((prev) => prev.concat({ id: new Date().getTime() }))
-            }}>
-            <FontAwesomeIcon icon={faAdd} />
-          </Button>
-          {newEmployees?.length > 0 && (
-            <section className='new-employees-list'>
-              {newEmployees.map(({ id }) => (
-                <EmployeeCard
-                  key={id}
-                  id={id}
-                  onDelete={() => {
-                    setNewEmployees((prev) => prev.filter((employee) => employee.id !== id))
-                  }}
-                  initialValues={{ name: '', email: '', role: undefined }}
-                  initialMode='edit'
-                  onSave={handleSave}
-                />
-              ))}
-            </section>
-          )}
-        </ConfigProvider>
-      </section>
+      <NewEmployees>
+        <Button
+          style={{ boxShadow: 'none' }}
+          type='primary'
+          onClick={handleAdd}>
+          <FontAwesomeIcon icon={faAdd} />
+        </Button>
+        {newEmployees?.length > 0 && (
+          <section className='new-employees-list'>
+            {newEmployees.map(({ id }) => (
+              <EmployeeCard
+                key={id}
+                id={id}
+                onDelete={() => {
+                  setNewEmployees((prev) => prev.filter((employee) => employee.id !== id))
+                }}
+                initialValues={{ name: '', email: '', role: undefined }}
+                initialMode='edit'
+                onSave={handleSave}
+              />
+            ))}
+          </section>
+        )}
+      </NewEmployees>
     </main>
   )
 }

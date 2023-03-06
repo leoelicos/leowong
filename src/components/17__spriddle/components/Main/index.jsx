89c84e9ac86913b3e50b00c9fc71303a02947770
Leo Wong <leoelicos@gmail.com>
2023-03-06 21:58:27 +1100

    Update import links. Add style

--- src/components/17__spriddle/components/Main/index.jsx
+++ src/components/17__spriddle/components/Main/index.jsx
@@ -1,17 +1,29 @@
-import Splash from './Splash'
-import PreGame from './PreGame'
-import Game from './Game'
-import PostGame from './PostGame'
+import Splash from './Splash/index.jsx'
+import PreGame from './PreGame/index.jsx'
+import Game from './Game/index.jsx'
+import PostGame from './PostGame/index.jsx'
 
 const Main = ({ stage, goToNextStage }) => {
-  return stage === 0 ? ( //
-    <Splash goToNextStage={goToNextStage} />
-  ) : stage === 1 ? (
-    <PreGame goToNextStage={goToNextStage} />
-  ) : stage === 2 ? (
-    <Game goToNextStage={goToNextStage} />
-  ) : (
-    <PostGame goToNextStage={goToNextStage} />
+  console.log({ stage })
+  const style = {
+    backgroundColor: '#eee',
+    flex: '1',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  }
+  return (
+    <main style={style}>
+      {stage === 0 ? ( //
+        <Splash goToNextStage={goToNextStage} />
+      ) : stage === 1 ? (
+        <PreGame goToNextStage={goToNextStage} />
+      ) : stage === 2 ? (
+        <Game goToNextStage={goToNextStage} />
+      ) : (
+        <PostGame goToNextStage={goToNextStage} />
+      )}
+    </main>
   )
 }
 export default Main

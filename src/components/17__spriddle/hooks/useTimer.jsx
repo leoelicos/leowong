2fb0c6705bd057558954b7df72f9ec76f95bf6a1
Leo Wong <leoelicos@gmail.com>
2023-03-07 23:30:36 +1100

    startTimer and endTimer

---
+++ src/components/17__spriddle/hooks/useTimer.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from 'react'
+
+const useTimer = ({ initialTime }) => {
+  // console.log({ initialTime })
+  const [time, setTime] = useState(initialTime)
+  let timer = useRef(null)
+
+  const startTimer = () => {
+    // console.log('useTimer startTimer')
+    setTime(initialTime)
+    timer.current = setInterval(() => {
+      setTime((prev) => {
+        // console.log('time', prev)
+        if (prev === 0) {
+          endTimer()
+          return prev
+        }
+        return prev - 1
+      })
+    }, 1000)
+  }
+
+  const endTimer = () => {
+    clearInterval(timer.current)
+  }
+
+  /* cleanup */
+  useEffect(() => {
+    return () => {
+      clearInterval(timer.current)
+    }
+  }, [])
+  return { time, startTimer, endTimer }
+}
+export default useTimer

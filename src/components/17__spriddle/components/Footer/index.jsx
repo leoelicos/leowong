179e240e3622d489bc03c5a99a4feebfea1a1208
Leo Wong <leoelicos@gmail.com>
2023-03-07 23:26:56 +1100

    Define footer style

---
+++ src/components/17__spriddle/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import { faReact } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Footer = () => {
+  const style = {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '2px 0'
+  }
+  return (
+    <footer style={style}>
+      <FontAwesomeIcon icon={faReact} />
+    </footer>
+  )
+}
+export default Footer

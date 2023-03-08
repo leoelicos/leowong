d074697bee0542d1d9a7edc07fd752fe92855663
Leo Wong <leoelicos@gmail.com>
2023-03-09 02:51:54 +1100

    Move page handling to Main component

--- src/components/17__spriddle/index.jsx
+++ src/components/17__spriddle/index.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
 import Header from './components/Header/index.jsx'
 import Footer from './components/Footer/index.jsx'
 import Main from './components/Main/index.jsx'
 
 export default function Spriddle() {
-  const [stage, setStage] = useState(0)
-
-  const goToNextStage = () => {
-    setStage((prev) => (prev + 1 > 3 ? 0 : prev + 1))
-  }
-
   const bodyStyle = {
     display: 'flex',
     flexDirection: 'column',
@@ -24,10 +17,7 @@ export default function Spriddle() {
         className='body'
         style={bodyStyle}>
         <Header />
-        <Main
-          stage={stage}
-          goToNextStage={goToNextStage}
-        />
+        <Main />
         <Footer />
       </div>
     </div>

48221e0c128a4dc80a7a10c09ac9c1460d306a98
Leo Wong <leoelicos@gmail.com>
2023-03-09 23:47:22 +1100

    Move rendering logic here

--- src/components/17__spriddle/components/Main/components/Game/index.jsx
+++ src/components/17__spriddle/components/Main/components/Game/index.jsx
@@ -1,26 +1,69 @@
-import Keyboard from './components/Keyboard'
 import RiddleQuestion from './components/RiddleQuestion'
 import RiddleAnswer from './components/RiddleAnswer'
-const Game = ({ riddleQuestion, attempt, attemptMemo, handleClickKey, time, health, finished }) => {
-  console.log({ attempt })
+import MainHeader from '../MainHeader'
+import MainFooter from '../MainFooter'
+import Keyboard from './components/Keyboard'
+import Row from './components/Row'
+import Key from './components/Key'
+const Game = ({ riddleQuestion, attempt, attemptMemo, handleClickKey, gameTime, health }) => {
   return (
-    <div>
-      <header>
-        <h1>
-          Time: {time}, Health: {health}, Finished: {finished ? 'True' : 'False'}
-        </h1>
-      </header>
-      <main>
-        <RiddleQuestion>{riddleQuestion}</RiddleQuestion>
+    <>
+      <MainHeader
+        time={gameTime}
+        health={health}
+      />
 
-        <RiddleAnswer>{attempt}</RiddleAnswer>
-        <Keyboard
-          keys={attemptMemo}
-          handleClickKey={handleClickKey}
-        />
-        {finished && 'Game finished'}
-      </main>
-    </div>
+      <RiddleQuestion>
+        {riddleQuestion.map((sentence) => (
+          <p
+            className='riddle-paragraph'
+            key={sentence}>
+            {sentence}
+          </p>
+        ))}
+      </RiddleQuestion>
+
+      <RiddleAnswer>x{attempt}x</RiddleAnswer>
+
+      <MainFooter>
+        <Keyboard>
+          <Row>
+            {'qwertyuiop'.split('').map((letter) => (
+              <Key
+                key={letter}
+                char={letter}
+                stage={attemptMemo[letter]}
+                handleClickKey={handleClickKey}
+              />
+            ))}
+          </Row>
+          <Row>
+            <div style={{ flex: '0.5' }} />
+            {'asdfghjkl'.split('').map((letter) => (
+              <Key
+                key={letter}
+                char={letter}
+                stage={attemptMemo[letter]}
+                handleClickKey={handleClickKey}
+              />
+            ))}
+            <div style={{ flex: '0.5' }} />
+          </Row>
+          <Row>
+            <div style={{ flex: '1' }} />
+            {'zxcvbnm'.split('').map((letter) => (
+              <Key
+                key={letter}
+                char={letter}
+                stage={attemptMemo[letter]}
+                handleClickKey={handleClickKey}
+              />
+            ))}
+            <div style={{ flex: '2' }} />
+          </Row>
+        </Keyboard>
+      </MainFooter>
+    </>
   )
 }
 

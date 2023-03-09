837ff1ec3cf524fb955e4bf9b9e0c4d21c418256
Leo Wong <leoelicos@gmail.com>
2023-03-09 23:46:48 +1100

    Add custom inline style and class

--- src/components/17__spriddle/components/Main/components/Game/components/RiddleQuestion.jsx
+++ src/components/17__spriddle/components/Main/components/Game/components/RiddleQuestion.jsx
@@ -1,5 +1,19 @@
 const RiddleQuestion = ({ children }) => {
-  const style = { backgroundColor: 'yellow' }
-  return <div style={style}>{children}</div>
+  const style = {
+    boxShadow: 'rgba(22,22, 22, 0.5) 0px 0px 4px 2px inset',
+
+    borderRadius: '12px',
+
+    whiteSpace: 'break-spaces',
+    padding: '16px',
+    backgroundColor: 'white'
+  }
+  return (
+    <div
+      style={style}
+      className='poem'>
+      {children}
+    </div>
+  )
 }
 export default RiddleQuestion

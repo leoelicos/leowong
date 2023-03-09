0ad1eb97a2b9be31668894a0602e80afcc06b946
Leo Wong <leoelicos@gmail.com>
2023-03-09 23:48:44 +1100

    Add outcome style

--- src/components/17__spriddle/components/Main/components/PostGame/index.jsx
+++ src/components/17__spriddle/components/Main/components/PostGame/index.jsx
@@ -1,18 +1,42 @@
 import { Button } from 'antd'
+import RiddleAnswer from '../Game/components/RiddleAnswer'
+import RiddleQuestion from '../Game/components/RiddleQuestion'
+import MainFooter from '../MainFooter'
+import MainHeader from '../MainHeader'
 
-const PostGame = ({ handlePostGameClickNewGame, outcome }) => {
+const PostGame = ({ handlePostGameClickNewGame, outcome, riddleQuestion, riddleAnswer, gameTime, health }) => {
+  const outcomeTitle = outcome === 0 ? 'YOU WIN' : 'YOU LOSE'
+  const outcomeParagraph = outcome === 0 ? 'you guessed all the letters' : outcome === 1 ? 'out of time' : 'out of guesses'
   return (
-    <div>
-      <header>{outcome === 0 ? 'You won!' : outcome === 1 ? 'You lose - out of time!' : 'You lose - out of guesses'}</header>
-      <main>Outcome {outcome}</main>
-      <footer>
+    <>
+      <MainHeader
+        time={gameTime}
+        health={health}
+      />
+
+      <h2 style={{ margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Open Sans, sans-serif' }}>{outcomeTitle}</h2>
+      <p style={{ margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Open Sans, sans-serif' }}>{outcomeParagraph}</p>
+
+      <RiddleQuestion>
+        {riddleQuestion.map((sentence) => (
+          <p
+            className='riddle-paragraph'
+            key={sentence}>
+            {sentence}
+          </p>
+        ))}
+      </RiddleQuestion>
+      <p style={{ margin: '0' }}>Answer:</p>
+      <RiddleAnswer>{riddleAnswer}</RiddleAnswer>
+
+      <MainFooter>
         <Button
           type='primary'
           onClick={handlePostGameClickNewGame}>
           New Game
         </Button>
-      </footer>
-    </div>
+      </MainFooter>
+    </>
   )
 }
 export default PostGame

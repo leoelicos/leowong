ad5fb0be888f1996d12bb3c9fa1a70e6519a4045
Leo Wong <leoelicos@gmail.com>
2023-03-10 03:19:37 +1100

    Remove comments

--- src/components/17__spriddle/api/useRiddle.jsx
+++ src/components/17__spriddle/api/useRiddle.jsx
@@ -13,28 +13,15 @@ const useRiddle = () => {
       res(data[Math.floor(Math.random() * data.length)])
     })
 
-  // const deserialiseRiddle = (data) => data.json()
-
   const spacingRiddle = (str) => str.replace(/([.!?:;])\s+/g, '$1###').split('###')
 
   const riddleUpdate = useCallback(async () => {
     let riddle = undefined
-    let data = undefined
 
     try {
       setRiddleLoading(true)
-
-      // while (riddle === undefined || riddle.answer.length > 12) {
-      // data = await fetchRiddle()
-      // if (!data) throw new Error('fetch error')
-
-      // riddle = await deserialiseRiddle(data)
-      // if (!riddle) throw new Error()
-
-      // console.log('Search', { riddle })
-      // }
       riddle = await fetchRiddle()
-      console.log('Final', { riddle })
+      // console.log('Final', { riddle })
       setRiddleQuestion(spacingRiddle(riddle.riddle))
       setRiddleAnswer(riddle.answer.toLowerCase())
     } catch (error) {

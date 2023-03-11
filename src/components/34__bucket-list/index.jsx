da746ca31ee767a5abeb1163d42f93e85b6488f4
Leo Wong <leoelicos@gmail.com>
2023-03-11 17:27:44 +1100

    Implement bucket logic with add, remove, edit

--- src/components/34__bucket-list/index.jsx
+++ src/components/34__bucket-list/index.jsx
@@ -1,3 +1,49 @@
-export default function BucketList() {
-  return <div>BucketList</div>
+import { useState } from 'react'
+import BucketForm from './components/BucketForm'
+import Bucket from './components/Bucket'
+
+import './style/index.css'
+
+function BucketList() {
+  const [bucket, setBucket] = useState([])
+
+  const addBucketItem = (item) => {
+    setBucket((prev) => prev.concat(item))
+  }
+
+  const completeBucketItem = (id) => {
+    setBucket((prev) =>
+      prev.map((item) => {
+        return item.id === id ? { ...item, isComplete: !item.isComplete } : item
+      })
+    )
+  }
+
+  const removeBucketItem = (id) => {
+    setBucket((prev) => prev.filter((item) => item.id !== id))
+  }
+
+  const editBucketItem = (id, item) => {
+    if (!item.text.length > 0) return
+    setBucket((prev) => prev.map((i) => (i.id === id ? { ...i, ...item } : i)))
+  }
+
+  return (
+    <div className='app-34'>
+      <div className='body'>
+        <div className='bucket-app'>
+          <h1>What is on your bucket list?</h1>
+          <BucketForm onSubmit={addBucketItem} />
+          <Bucket
+            bucket={bucket}
+            completeBucketItem={completeBucketItem}
+            removeBucketItem={removeBucketItem}
+            editBucketItem={editBucketItem}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default BucketList

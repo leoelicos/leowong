970b580e00ac977a27f779fa3017cd535c637e5d
Leo Wong <leoelicos@gmail.com>
2023-03-12 02:04:11 +1100

    Add link to deployed

--- src/components/19__note-taker/index.jsx
+++ src/components/19__note-taker/index.jsx
@@ -1,3 +1,13 @@
 export default function NoteTaker() {
-  return <div>NoteTaker</div>
+  return (
+    <div>
+      <p>NoteTaker</p>
+      <a
+        href='https://leoelicos-note-taker.herokuapp.com/'
+        target='_blank'
+        rel='noopener'>
+        Deployed
+      </a>
+    </div>
+  )
 }

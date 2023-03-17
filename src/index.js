45df6fe05779f79f47073e5005cc998e01c2bc9d
Leo Wong <leoelicos@gmail.com>
2023-03-17 22:06:42 +1100

    Update ErrorPage route. Render child routes from data. Add fallbackElement to RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+import { Spin } from 'antd'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Toc from './admin/TOC/index'
-import ErrorPage from './admin/ErrorPage/index'
+import ErrorPage from './components/ErrorPage/index.jsx'
 import apps from './data/apps'
 
 import './style/index.css'
 
-const appRoutes = apps.map(({ link, element }) => ({ path: link, element, errorElement: <ErrorPage /> }))
+const appRoutes = apps.map(({ link, element, children = [] }) => ({ path: link, element, children: children, errorElement: <ErrorPage /> }))
 const routes = [{ path: '/', element: <Toc />, errorElement: <ErrorPage /> }, ...appRoutes]
 const router = createBrowserRouter(routes)
 const rootEl = document.getElementById('root')
 
-createRoot(rootEl).render(<RouterProvider router={router} />)
+createRoot(rootEl).render(
+  <RouterProvider
+    fallbackElement={<Spin />}
+    router={router}
+  />
+)

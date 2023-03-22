e99dbae0be6d343fb5ea9b7ae4cf8b0291960b8f
Leo Wong <leoelicos@gmail.com>
2023-03-23 01:44:06 +1100

    Move routing logic to router/index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
 import { Spin } from 'antd'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import TOC from './components/TOC/index.jsx'
-import ErrorPage from './components/ErrorPage/index.jsx'
-
-import apps from './data/apps.js'
+import { RouterProvider } from 'react-router-dom'
 
 import './style/index.css'
 
-const appRoutes = apps.map(
-  ({
-    link,
-    element,
-    children = [] //
-  }) => ({
-    path: link,
-    element,
-    children: children,
-    errorElement: <ErrorPage />
-  })
-)
-const routes = [
-  {
-    path: '/',
-    element: <TOC />,
-    errorElement: <ErrorPage />
-  },
-  ...appRoutes
-]
-const router = createBrowserRouter(routes)
+import router from './router/index.js'
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider

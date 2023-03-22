ed08a6ac53b8ca7bf5a03cdf9d1d4e8ae3119423
Leo Wong <leoelicos@gmail.com>
2023-03-23 01:40:48 +1100

    Remove react import. Import TOC and ErrorPage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 import { Spin } from 'antd'
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import components from './components/index.js'
-import apps from './data/apps'
+import TOC from './components/TOC/index.jsx'
+import ErrorPage from './components/ErrorPage/index.jsx'
+
+import apps from './data/apps.js'
 
 import './style/index.css'
 
-const appRoutes = apps.map(({ link, element, children = [] }) => ({ path: link, element, children: children, errorElement: <ErrorPage /> }))
-const routes = [{ path: '/', element: <components.TOC />, errorElement: <components.ErrorPage /> }, ...appRoutes]
+const appRoutes = apps.map(
+  ({
+    link,
+    element,
+    children = [] //
+  }) => ({
+    path: link,
+    element,
+    children: children,
+    errorElement: <ErrorPage />
+  })
+)
+const routes = [
+  {
+    path: '/',
+    element: <TOC />,
+    errorElement: <ErrorPage />
+  },
+  ...appRoutes
+]
 const router = createBrowserRouter(routes)
-const rootEl = document.getElementById('root')
 
-createRoot(rootEl).render(
+createRoot(document.getElementById('root')).render(
   <RouterProvider
     fallbackElement={<Spin />}
     router={router}

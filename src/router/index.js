9b6ab4cc65155c16281d002daf56ae184bad2ed7
Leo Wong <leoelicos@gmail.com>
2023-03-24 01:16:52 +1100

    Import Projects and Resume

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,31 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 
 import Home from '../pages/Home/index.jsx'
 import ErrorPage from '../components/ErrorPage/index.jsx'
 
 import About from '../pages/Home/components/About/index.jsx'
+import Projects from '../pages/Home/components/Projects/index.jsx'
+import Resume from '../pages/Home/components/Resume/index.jsx'
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     element: <Home />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path: '/about',
-        element: <About />,
-        errorElement: <ErrorPage />
-      }
+      { path: '/home/about', element: <About />, errorElement: <ErrorPage /> },
+      { path: '/home/projects', element: <Projects />, errorElement: <ErrorPage /> },
+      { path: '/home/resume', element: <Resume />, errorElement: <ErrorPage /> }
     ]
+  },
+  {
+    path: '*',
+    element: (
+      <Navigate
+        to='/home'
+        replace
+      />
+    )
   }
 ]
 

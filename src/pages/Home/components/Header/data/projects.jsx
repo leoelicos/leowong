768d8e3049a7a2907ae30209a6cfeaaa30add9e6
Leo Wong <leoelicos@gmail.com>
2023-03-23 23:59:00 +1100

    PastelProfile renamed as Header

---
+++ src/pages/Home/components/Header/data/projects.jsx
@@ -0,0 +1,52 @@
+const data = [
+  {
+    title: 'MOOVEE',
+    description: 'YouTube Trailer Search',
+    deployedURL: 'https://leoelicos.github.io/bcs-07-moovee/',
+    repoURL: 'https://github.com/leoelicos/bcs-07-moovee',
+    imageURL: 'https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/project/moovee.png'
+  },
+  {
+    title: 'Just Kidding!',
+    description: 'Image Sharing Forum',
+    deployedURL: 'https://just-kidding-baby.herokuapp.com/',
+    repoURL: 'https://github.com/leoelicos/bcs-14-tech-blog',
+    imageURL: 'https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/project/jk.jpg'
+  },
+  {
+    title: 'Submitted!',
+    description: 'Wordpress-style Tech Blog',
+    deployedURL: 'https://leoelicos-tech-blog.herokuapp.com/',
+    repoURL: 'https://github.com/leoelicos/bcs-14-tech-blog',
+    imageURL: 'https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/project/submitted.jpg'
+  },
+  {
+    title: 'Employee Management System',
+    description: 'Command Line CMS',
+    deployedURL: 'https://www.youtube.com/watch?v=W58SFkcsXkM',
+    repoURL: 'https://github.com/leoelicos/bcs-12-employee-management-system/',
+    imageURL: 'https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/project/ems.jpg'
+  },
+  {
+    title: 'WWAG',
+    description: 'Weather Data Visualization',
+    deployedURL: 'https://leoelicos.github.io/bcs-06-weather-dashboard/',
+    repoURL: 'https://github.com/leoelicos/bcs-06-weather-dashboard',
+    imageURL: 'https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/project/wwag.jpg'
+  },
+  {
+    title: 'SYTYCJ',
+    description: 'Timed Javascript Quiz',
+    repoURL: 'https://github.com/leoelicos/bcs-04-code-quiz',
+    deployedURL: 'https://leoelicos.github.io/bcs-04-code-quiz/',
+    imageURL: 'https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/project/sytycj.jpg'
+  },
+  {
+    title: 'KIDDLE',
+    description: 'Wordle for kids',
+    repoURL: 'https://github.com/leoelicos/bcs-04-word-guess',
+    deployedURL: 'https://leoelicos.github.io/bcs-04-word-guess/',
+    imageURL: 'https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/project/kiddle.jpg'
+  }
+]
+export default data
---
+++ src/pages/Home/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import PastelProfile from './PastelProfile.jsx'
+
+const App = () => {
+  return (
+    <div className='app-35'>
+      <div className='body'>
+        <PastelProfile />
+      </div>
+    </div>
+  )
+}
+export default App

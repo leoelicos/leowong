0672a6cf78d33f2bc8c49e4bcdbe7b621a1d8ecc
Leo Wong <leoelicos@gmail.com>
2023-03-24 00:47:19 +1100

    Remove useEffect

--- src/pages/Home/components/Background/index.jsx
+++ src/pages/Home/components/Background/index.jsx
@@ -1,33 +1,5 @@
-import { useState, useEffect } from 'react'
 import './style/style.css'
 
 export default function Background({ children }) {
-  const [bgLoaded, setBgLoaded] = useState(false)
-
-  useEffect(() => {
-    const img = new Image()
-    img.onload = () => {
-      setBgLoaded(true)
-    }
-    img.src = './images/splash.jpg'
-  }, [])
-
-  const backgroundStyle = {
-    backgroundImage: bgLoaded ? 'url("../images/splash.jpg")' : 'none',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: -1
-  }
-  return (
-    <div
-      className='home background'
-      style={backgroundStyle}>
-      {children}
-    </div>
-  )
+  return <div className='background'>{children}</div>
 }

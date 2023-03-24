1404a53e1a36b221cf61d5d5c9f66de2ea801978
Leo Wong <leoelicos@gmail.com>
2023-03-25 04:38:56 +1100

    Information page for Submitted

--- src/pages/Submitted/index.jsx
+++ src/pages/Submitted/index.jsx
@@ -1,3 +1,7 @@
+import useTitle from '../../hooks/useTitle'
+import useFavicon from '../../hooks/useFavicon'
 export default function Submitted() {
+  useTitle('submitted!')
+  useFavicon('favicons/submitted.png')
   return <div>Submitted</div>
 }

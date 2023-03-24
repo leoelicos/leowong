160726610f7cd27e6b6d663dd7138df6590e3f64
Leo Wong <leoelicos@gmail.com>
2023-03-25 04:36:33 +1100

    Add favicon and title

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,19 @@
+import useFavicon from '../../hooks/useFavicon.js'
+import useTitle from '../../hooks/useTitle.js'
 import Header from './components/1__Header/index.jsx'
 import Hero from './components/2__Hero/index.jsx'
 import About from './components/3__About/index.jsx'
 import Projects from './components/4__Projects/index.jsx'
 import Resume from './components/5__Resume/index.jsx'
 
-import './style/style.css'
+import './style.css'
 export default function Home() {
+  useFavicon('favicons/00-favicon.png')
+  useTitle('Leo Wong')
   return (
     <div className='home'>
       <Header />
-      <div class='home-content'>
+      <div className='home-content'>
         <Hero>
           <About />
           <Projects />

c24bf5f9e9db1f427c05118072d18e3f8123d726
Leo Wong <leoelicos@gmail.com>
2023-03-25 04:38:40 +1100

    Information page for ingre

--- src/pages/Ingre/index.jsx
+++ src/pages/Ingre/index.jsx
@@ -1,3 +1,8 @@
+import useTitle from '../../hooks/useTitle'
+import useFavicon from '../../hooks/useFavicon'
+
 export default function Ingre() {
-  return <div>Ingre</div>
+  useTitle('ingré')
+  useFavicon('favicons/40-favicon.png')
+  return null
 }

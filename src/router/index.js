d8a805bfbab6f0fab1f938c100fb241dc4b32519
Leo Wong <leoelicos@gmail.com>
2023-03-25 04:37:55 +1100

    Update app paths

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,47 @@ import { createBrowserRouter, Navigate } from 'react-router-dom'
 import Home from '../pages/Home/index.jsx'
 import ErrorPage from '../components/ErrorPage/index.jsx'
 
+import Ingre from '../pages/Ingre/index.jsx'
+import Submitted from '../pages/Submitted/index.jsx'
+import Algoz from '../pages/Algoz/index.jsx'
+import Clima from '../pages/Clima/index.jsx'
+import Kiddin from '../pages/Kiddin/index.jsx'
+import MooVee from '../pages/Moovee/index.jsx'
+
 const routes = [
   {
     path: '/',
     element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/ingre',
+    element: <Ingre />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/submitted',
+    element: <Submitted />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/algoz',
+    element: <Algoz />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/kiddin',
+    element: <Kiddin />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/moovee',
+    element: <MooVee />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/clima',
+    element: <Clima />,
     errorElement: <ErrorPage />
   },
   {

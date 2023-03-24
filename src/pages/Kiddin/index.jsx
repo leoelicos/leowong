224c87dcf209d3112b877715c28764e5fa0dedf0
Leo Wong <leoelicos@gmail.com>
2023-03-25 04:38:49 +1100

    Information page for Kiddle

--- src/pages/Kiddin/index.jsx
+++ src/pages/Kiddin/index.jsx
@@ -1,3 +1,8 @@
+import useTitle from '../../hooks/useTitle'
+import useFavicon from '../../hooks/useFavicon'
+
 export default function JustKidding() {
+  useTitle('Kiddin')
+  useFavicon('/favicons/27-favicon.png')
   return <div>JustKidding</div>
 }

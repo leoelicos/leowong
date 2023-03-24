43016b66ec9a8939f9f2679d8f101d30da383e7d
Leo Wong <leoelicos@gmail.com>
2023-03-24 23:49:08 +1100

    Replaced Deeply nested components with simpler code

---
+++ src/pages/Home/components/1__Header/index.jsx
@@ -0,0 +1,38 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import './index.css'
+export default function Header() {
+  const [showMenu, setShowMenu] = useState(true)
+
+  const toggle = () => setShowMenu((m) => !m)
+
+  return (
+    <header>
+      <a
+        className='logo'
+        href='#top'>
+        LEOWONG
+      </a>
+
+      <nav className='nav'>
+        <ul className='menu small'>
+          <a href='#about'>ABOUT</a>
+          <a href='#projects'>PROJECTS</a>
+          <a href='#resume'>RESUME</a>
+        </ul>
+
+        <button
+          className='hamburger'
+          onClick={toggle}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <ul className={`menu big ${showMenu ? 'hide' : ''}`}>
+          <a href='/#about'>ABOUT</a>
+          <a href='/#projects'>PROJECTS</a>
+          <a href='/#resume'>RESUME</a>
+        </ul>
+      </nav>
+    </header>
+  )
+}

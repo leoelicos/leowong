5bfa5e6fc6dd430585ac9f3c073c39c4d21e3101
Leo Wong <leoelicos@gmail.com>
2023-03-24 23:52:30 +1100

    Rework Home component to have no Outlets, but a single scrolling page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,21 @@
-import { Outlet } from 'react-router-dom'
-import Background from './components/Background/index.jsx'
-import Header from './components/Header/index.jsx'
-import './style/style.css'
+import Header from './components/1__Header/index.jsx'
+import Hero from './components/2__Hero/index.jsx'
+import About from './components/3__About/index.jsx'
+import Projects from './components/4__Projects/index.jsx'
+import Resume from './components/5__Resume/index.jsx'
 
+import './style/style.css'
 export default function Home() {
   return (
     <div className='home'>
-      <Background>
-        <Header />
-        <Outlet />
-      </Background>
+      <Header />
+      <div class='home-content'>
+        <Hero>
+          <About />
+          <Projects />
+          <Resume />
+        </Hero>
+      </div>
     </div>
   )
 }

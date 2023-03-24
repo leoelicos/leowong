a4b517c6f623efe18c364249e61bfdc49323b909
Leo Wong <leoelicos@gmail.com>
2023-03-24 23:52:58 +1100

    Reduce Homepage to just a single Route with no children

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,17 @@ import { createBrowserRouter, Navigate } from 'react-router-dom'
 import Home from '../pages/Home/index.jsx'
 import ErrorPage from '../components/ErrorPage/index.jsx'
 
-import About from '../pages/Home/components/About/index.jsx'
-import Projects from '../pages/Home/components/Projects/index.jsx'
-import Resume from '../pages/Home/components/Resume/index.jsx'
-
 const routes = [
   {
-    path: '/home',
+    path: '/',
     element: <Home />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/home/about', element: <About />, errorElement: <ErrorPage /> },
-      { path: '/home/projects', element: <Projects />, errorElement: <ErrorPage /> },
-      { path: '/home/resume', element: <Resume />, errorElement: <ErrorPage /> }
-    ]
+    errorElement: <ErrorPage />
   },
   {
     path: '*',
     element: (
       <Navigate
-        to='/home'
+        to='/'
         replace
       />
     )

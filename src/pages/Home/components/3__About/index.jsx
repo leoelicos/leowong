0c4fe536e00591068ef2dbb4644fe390639c6c21
Leo Wong <leoelicos@gmail.com>
2023-03-25 04:35:18 +1100

    Add Bio

--- src/pages/Home/components/3__About/index.jsx
+++ src/pages/Home/components/3__About/index.jsx
@@ -1,46 +1,38 @@
 /* custom images */
 import Avatar from '../../images/avatar.png'
-import './index.css'
+import './style.css'
 export default function About() {
   return (
-    <section className='scrolling'>
-      <section
-        className='about-me'
-        id='about'>
-        <img
-          className='avatar'
-          src={Avatar}
-          alt='leo wong'
-        />
-        <article className='bio'>
-          <h3>Leo Wong</h3>
-          <p>Full stack developer</p>
+    <section
+      className='about-me'
+      id='about'>
+      <img
+        className='avatar'
+        src={Avatar}
+        alt='leo wong'
+      />
+      <h3>About Leo Wong</h3>
+      <h4>Full stack developer</h4>
+      <p>Leo is a passionate developer who has spent a year studying both front-end and back-end coding. His love for coding started when he did an intensive coding bootcamp with the University of Sydney. He soon realized his love for React, building and designing user interfaces that are not only functional but also aesthetically pleasing.</p>
 
-          <p>Leo is a passionate developer who has spent a year studying both front-end and back-end coding. His love for coding started when he did an intensive coding bootcamp with the University of Sydney. He soon realized his love for React, building and designing user interfaces that are not only functional but also aesthetically pleasing.</p>
-        </article>
-        <article className='front-end-skills'>
-          <h3>Front end</h3>
-          <p>
-            <span className='hashtag'>React</span>
-            <span className='hashtag'>AntD</span>
-            <br />
-            <span className='hashtag'>jQuery</span>
-            <span className='hashtag'>HTML</span>
-            <span className='hashtag'>CSS</span>
-          </p>
-        </article>
-        <article className='back-end-skills'>
-          <h3>Back end</h3>
-          <p>
-            <span className='hashtag'>Express</span>
-            <br />
-            <span className='hashtag'>SQL</span>
-            <span className='hashtag'>MongoQL</span>
-            <span className='hashtag'>GQL</span>
-          </p>
-        </article>
-      </section>
-      <section className='articles'></section>
+      <h4>Front end</h4>
+      <p className='hashtags'>
+        <span className='hashtag'>React</span>
+        <span className='hashtag'>AntD</span>
+        <span className='hashtag'>jQuery</span>
+        <span className='hashtag'>HTML</span>
+        <span className='hashtag'>CSS</span>
+        <span className='hashtag'>Bootstrap</span>
+        <span className='hashtag'>Materialize</span>
+      </p>
+
+      <h4>Back end</h4>
+      <p className='hashtags'>
+        <span className='hashtag'>Express</span>
+        <span className='hashtag'>SQL</span>
+        <span className='hashtag'>MongoQL</span>
+        <span className='hashtag'>GQL</span>
+      </p>
     </section>
   )
 }

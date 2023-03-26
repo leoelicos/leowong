ead9203e9779c8810b848564fb502e30442a6ed8
Leo Wong <leoelicos@gmail.com>
2023-03-26 21:37:10 +1100

    Simplify routes while I work out how to use createRoutesFromElements

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,24 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route //
+} from 'react-router-dom'
 
-import Home from '../pages/Home/index.jsx'
 import ErrorPage from '../components/ErrorPage/index.jsx'
+import Home from '../pages/Home/index.jsx'
+import Moovee from '../pages/Moovee/index.jsx'
 
-import Ingre from '../pages/Ingre/index.jsx'
-import Submitted from '../pages/Submitted/index.jsx'
-import Algoz from '../pages/Algoz/index.jsx'
-import Clima from '../pages/Clima/index.jsx'
-import Kiddin from '../pages/Kiddin/index.jsx'
-import MooVee from '../pages/Moovee/index.jsx'
-
-const routes = [
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/ingre',
-    element: <Ingre />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/submitted',
-    element: <Submitted />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/algoz',
-    element: <Algoz />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/kiddin',
-    element: <Kiddin />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/moovee',
-    element: <MooVee />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/clima',
-    element: <Clima />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '*',
-    element: (
-      <Navigate
-        to='/'
-        replace
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={<Home />}>
+      <Route
+        path='moovee'
+        element={<Moovee />}
       />
-    )
-  }
-]
-
-const router = createBrowserRouter(routes)
+    </Route>
+  )
+)
 
 export default router

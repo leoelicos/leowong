b0b19207111caccdd372b391830fdd5207f9f334
Leo Wong <leoelicos@gmail.com>
2023-03-26 21:33:32 +1100

    Add StrictMode wrapper

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import { Spin } from 'antd'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-
-import './style/index.css'
+import { Spin } from 'antd'
 
 import router from './router/index.js'
 
+import './style/index.css'
+
 createRoot(document.getElementById('root')).render(
-  <RouterProvider
-    fallbackElement={<Spin />}
-    router={router}
-  />
+  <StrictMode>
+    <RouterProvider
+      fallbackElement={<Spin />}
+      router={router}
+    />
+  </StrictMode>
 )

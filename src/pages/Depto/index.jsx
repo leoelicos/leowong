e6d60f2423aaa0abdbd27156f4b348f498c40c10
Leo Wong <leoelicos@gmail.com>
2023-03-27 22:52:54 +1100

    Remove components that are actually deployed externally

--- src/pages/Depto/index.jsx
+++
@@ -1,5 +0,0 @@
-export default function Depto() {
-  useTitle('Depto')
-  useFavicon('favicons/depto.png')
-  return <div>Depto</div>
-}
--- src/pages/Grader/index.jsx
+++
@@ -1,8 +0,0 @@
-import useFavicon from '../../hooks/useFavicon'
-import useTitle from '../../hooks/useTitle'
-
-export default function Grader() {
-  useTitle('Grader')
-  useFavicon('favicons/grader.png')
-  return <div>Grader</div>
-}

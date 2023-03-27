1f4b24c0274c996d2adbc338b5152d05b37ebe64
Leo Wong <leoelicos@gmail.com>
2023-03-27 12:51:59 +1100

    Implement routing using createBrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { Spin } from 'antd'
-
-import router from './router/index.js'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider, //
+  Route
+} from 'react-router-dom'
 import './style/index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider
-      fallbackElement={<Spin />}
-      router={router}
+import ErrorPage from './components/ErrorPage/index.jsx'
+
+import Home from './pages/Home/index.jsx'
+
+import appData from './data/apps'
+import Description from './components/Description/index.jsx'
+
+console.log({ appData })
+const routes = []
+for (const app in appData) {
+  routes.push(
+    <Route
+      path={`/${appData[app].endpoint}`}
+      element={
+        <Description
+          app={appData[app]}
+          errorElement={<ErrorPage />}
+        />
+      }
     />
-  </StrictMode>
+  )
+}
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route
+      path='/'
+      element={<Home />}
+      errorElement={<ErrorPage />}
+    />,
+    ...routes
+  ])
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
+
+/* 
+
+alternative method to create router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/moovee',
+    element: <Moovee />,
+    errorElement: <ErrorPage />
+  }
+]) */

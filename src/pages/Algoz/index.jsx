41f5a1af6e23d862044a900bbd935e06b43183b9
Leo Wong <leoelicos@gmail.com>
2023-04-02 03:26:38 +1000

    Add a modal to render the cards

--- src/pages/Algoz/index.jsx
+++ src/pages/Algoz/index.jsx
@@ -2,10 +2,17 @@ import Prism from 'prismjs'
 import './style/algoz.scss'
 
 import algos from './data/data.json'
-import { useState } from 'react'
+import { createContext, useMemo, useState } from 'react'
 import useTitle from '../../hooks/useTitle'
 import useFavicon from '../../hooks/useFavicon'
 
+import { Button, ConfigProvider, Modal, notification } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faCopy } from '@fortawesome/free-regular-svg-icons'
+const Context = createContext({
+  name: 'Default'
+})
 function formatCode(code) {
   return Prism.highlight(code, Prism.languages.javascript, 'javascript')
 }
@@ -13,104 +20,154 @@ function formatCode(code) {
 export default function Algoz() {
   useTitle('Algoz')
   useFavicon('/favicons/algoz.png')
+  const [api, contextHolder] = notification.useNotification()
+
+  const openNotification = () => {
+    api.info({
+      //
+      message: 'Success!',
+      description: <Context.Consumer>{({ name }) => `${name}`}</Context.Consumer>,
+      placement: 'bottomRight',
+      duration: 0.5
+    })
+  }
+
+  const [difficulty, setDifficulty] = useState('medium')
+  const difficulties = ['easy', 'medium', 'hard']
+
+  /* antd modal */
+  const [algo, setAlgo] = useState(undefined)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+  const handleOk = () => {
+    setIsModalOpen(false)
+  }
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(algo.solution)
+      openNotification()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  const contextValue = useMemo(() => ({ name: 'Password copied to clipboard' }), [])
   return (
     <div className='algoz'>
-      <div
-        className='body'
-        style={{ width: '100%', height: '100%' }}>
+      <Context.Provider value={contextValue}>
+        {contextHolder}
         <header>
           <h1>Algoz</h1>
           <nav>
             <ul>
-              <li>
-                <button
-                  onClick={() => {
-                    console.log('Clicked Medium!')
-                  }}>
-                  Medium
-                </button>
-              </li>
+              {difficulties.map((d) => (
+                <li key={d}>
+                  <button
+                    className={`difficulty ${d === difficulty ? 'active' : ''}`}
+                    onClick={() => {
+                      setDifficulty(d)
+                      console.log(`Clicked ${d}`)
+                    }}>
+                    {d}
+                  </button>
+                </li>
+              ))}
             </ul>
           </nav>
         </header>
         <main>
           {algos
+            .filter((algo) => algo.difficulty === difficulty)
             .sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
             .map((algo) => (
               <ACard
                 algo={algo}
                 key={algo.name}
+                onClickAlgo={() => {
+                  setAlgo(algo)
+                  showModal()
+                }}
               />
             ))}
         </main>
-      </div>
+        {algo && (
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: 'green',
+                colorBgBase: 'black'
+              }
+            }}>
+            <Modal
+              closable={false}
+              footer={
+                <>
+                  <Button
+                    type='primary'
+                    onClick={handleCopy}>
+                    <FontAwesomeIcon icon={faCopy} />
+                    &nbsp;Copy
+                  </Button>
+                  <Button
+                    type='primary'
+                    onClick={handleOk}>
+                    OK
+                  </Button>
+                </>
+              }
+              title={<div style={{ color: 'white' }}>{algo.name}</div>}
+              open={isModalOpen}
+              onCancel={handleCancel}
+              bodyStyle={{ backgroundColor: 'black' }}
+              children={
+                <div className='algoz__code'>
+                  <pre>
+                    <code
+                      dangerouslySetInnerHTML={{
+                        __html: formatCode(algo.solution)
+                      }}
+                    />
+                  </pre>
+                </div>
+              }
+            />
+          </ConfigProvider>
+        )}
+      </Context.Provider>
     </div>
   )
 }
-const ACard = ({ algo }) => {
-  const [toggle, setToggle] = useState(false)
-  const handleToggle = () => {
-    setToggle((prev) => !prev)
-  }
-
+const ACard = ({ algo, onClickAlgo }) => {
   return (
-    <>
-      <div className='card'>
-        <div
-          className='name'
-          style={{
-            width: '100%',
-            whiteSpace: 'pre-wrap'
-          }}>
-          {algo.name}
-        </div>
-        <div className='buttons'>
-          <button
-            type='default'
-            onClick={() => {
-              window.open(algo.link, '_blank', 'noreferrer')
-            }}
-            className='button problem'>
-            Problem
-          </button>
-          <button
-            onClick={handleToggle}
-            className={`button solution ${toggle ? 'on' : 'off'}`}>
-            Solution
-          </button>
-        </div>
-      </div>
-      <div
-        style={{
-          margin: '-2rem 0 0 0',
-
-          maxHeight: toggle ? '9999px' : '0',
-          transition: toggle ? 'max-height 1s linear' : 'max-height 0s linear',
-          overflow: toggle ? 'unset' : 'hidden',
-
-          padding: '5px 0',
-          justifyContent: 'center'
+    <div className='card'>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorLink: 'black'
+          }
         }}>
-        <pre
-          className='language-js'
-          style={{
-            padding: '8px 8px',
-            borderRadius: '0 0 8px 8px',
-            width: '100%',
-            whiteSpace: 'break-spaces',
-            wordBreak: 'break-word'
-          }}>
-          <code
-            dangerouslySetInnerHTML={{ __html: formatCode(algo.solution) }}
-            className='language-js'
-            style={{
-              width: '100%',
-              whiteSpace: 'break-spaces',
-              wordBreak: 'break-word'
-            }}
-          />
-        </pre>
-      </div>
-    </>
+        <Button
+          block
+          type='link'
+          href={algo.link}
+          target='_blank'
+          rel='noreferrer'
+          className='leetcode-link'>
+          {algo.name}
+        </Button>
+      </ConfigProvider>
+      <Button
+        type='default'
+        block
+        onClick={onClickAlgo}
+        style={{ borderRadius: '0 0 16px 16px' }}>
+        <FontAwesomeIcon icon={faCode} />
+      </Button>
+    </div>
   )
 }

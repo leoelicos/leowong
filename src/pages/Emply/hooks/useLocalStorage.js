1bc7dfd88c3a6048a51a148b03c8d30d316871ce
Leo Wong <leoelicos@gmail.com>
2023-04-03 17:38:34 +1000

    Initialise useState with a function

--- src/pages/Emply/hooks/useLocalStorage.js
+++ src/pages/Emply/hooks/useLocalStorage.js
@@ -1,24 +1,21 @@
 import { useEffect, useState } from 'react'
 
 const useLocalStorage = (name) => {
-  const [employeesLS, setEmployeesLS] = useState()
+  const [ls, setLS] = useState(() => {
+    let item = JSON.parse(localStorage.getItem(name)) || []
+    return item
+  })
 
+  /* update local storage every time local state changes */
   useEffect(() => {
-    let item = localStorage.getItem(name)
-    if (item === 'undefined') item = []
-    else item = JSON.parse(item)
+    localStorage.setItem(name, JSON.stringify(ls))
+  }, [ls, name])
 
-    setEmployeesLS(item)
-  }, [name])
-
-  useEffect(() => {
-    localStorage.setItem(name, JSON.stringify(employeesLS))
-  }, [employeesLS, name])
-
-  const updateEmployeesLS = (r) => {
-    setEmployeesLS(r)
+  /* expose function to update local state */
+  const updateLS = (r) => {
+    setLS(r)
   }
 
-  return { employeesLS, updateEmployeesLS }
+  return { ls, updateLS }
 }
 export default useLocalStorage

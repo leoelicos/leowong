c4fd90284713927d7a50ac6725caa2fa598910d0
Leo Wong <leoelicos@gmail.com>
2023-04-11 23:51:00 +1000

    Delete unused code

--- src/pages/Kiddle/components/FooterButtons.jsx
+++ src/pages/Kiddle/components/FooterButtons.jsx
@@ -1,17 +1,1 @@
-const FooterButtons = ({ handleClickResetTally, handleClickHelp }) => (
-  <section className='footer-buttons'>
-    <button
-      className='btn'
-      id='button-reset-games'
-      onClick={handleClickResetTally}>
-      Reset
-    </button>
-    <button
-      className='btn'
-      id='button-help'
-      onClick={handleClickHelp}>
-      ?
-    </button>
-  </section>
-)
-export default FooterButtons
+ 

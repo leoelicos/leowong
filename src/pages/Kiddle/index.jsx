74d4762379c351394b83e0bfb51238eb676179ff
Leo Wong <leoelicos@gmail.com>
2023-04-14 21:52:17 +1000

    Replace footer with modal

--- src/pages/Kiddle/index.jsx
+++ src/pages/Kiddle/index.jsx
@@ -2,23 +2,20 @@
 import useFavicon from '../../hooks/useFavicon'
 import useTitle from '../../hooks/useTitle'
 
-/* data */
-
 /* components */
 import Alarm from './components/Alarm.jsx'
-import StartButton from './components/StartButton.jsx'
 import Tiles from './components/Tiles'
-import HelpModal from './components/HelpModal.jsx'
+import KiddleModal from './components/KiddleModal.jsx'
 
 /* style */
 import './style/index.css'
 import Elephant from './images/elephantOrange.png'
 import useGame from './hooks/useGame'
 import useModal from './hooks/useModal'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRotateLeft, faChartColumn } from '@fortawesome/free-solid-svg-icons'
 
 export default function WordGuess() {
-  console.log('render')
-
   useFavicon('/favicons/kiddle.png')
   useTitle('Kiddle')
 
@@ -35,14 +32,16 @@ export default function WordGuess() {
   } = useGame()
 
   const {
-    isModalOpen,
-    handleClickHelp,
-    handleClickModalOk,
-    handleClickModalCancel //
+    isModalOpen: isHelpOpen,
+    handleClickOpen: handleClickHelp,
+    handleClickModalOk: handleClickHelpOk,
+    handleClickModalCancel: handleClickHelpCancel //
   } = useModal()
 
+  const { isModalOpen: isStatsOpen, handleClickOpen: handleClickStatsOpen, handleClickModalOk: handleClickStatsOk, handleClickModalCancel: handleClickStatsCancel } = useModal()
+
   return (
-    <div className='kiddle'>
+    <div className='kiddle disable-caret'>
       <header>
         <div className='logo'>
           <img
@@ -50,33 +49,73 @@ export default function WordGuess() {
             alt='elephant logo'
           />
         </div>
-        <div>
-          <h1>KIDDLE</h1>
+        <h1>KIDDLE</h1>
+
+        <div className='extra'>
+          <button
+            id='stats'
+            onClick={handleClickStatsOpen}>
+            <FontAwesomeIcon icon={faChartColumn} />
+          </button>
+          <button
+            id='help'
+            onClick={handleClickHelp}>
+            ?
+          </button>
         </div>
-        <Alarm seconds={seconds} />
-        <StartButton
-          disabled={!started}
-          handleClickStart={startGame}
-          buttonMessage={buttonMessage}
-        />
       </header>
 
       <main>
         <Tiles tiles={tiles} />
+        {started ? (
+          <Alarm seconds={seconds} />
+        ) : (
+          <button
+            type='button'
+            onClick={() => {
+              startGame()
+            }}>
+            Start
+          </button>
+        )}
       </main>
 
-      <footer>
-        <button id='button-tally-wins'>Wins: {tally?.wins}</button>
-        <button id='button-tally-losses'>Losses: {tally?.losses}</button>
-        <button onClick={handleClickResetTally}>Reset</button>
-        <button onClick={handleClickHelp}>?</button>
+      {/* Stats modal */}
+      <KiddleModal
+        handleClickModalOk={handleClickStatsOk}
+        handleClickModalCancel={handleClickStatsCancel}
+        isModalOpen={isStatsOpen}
+        title='Stats'>
+        <div>Wins: {tally?.wins}</div>
+        <div>Losses: {tally?.losses}</div>
+        <button
+          type='button'
+          onClick={handleClickResetTally}
+          style={{
+            color: 'black',
+            backgroundColor: 'transparent',
+            borderRadius: '1vmin',
+            border: 'none',
+            width: 'fit-content',
+            cursor: 'pointer',
+            margin: 'auto'
+          }}>
+          <FontAwesomeIcon icon={faArrowRotateLeft} />
+        </button>
+      </KiddleModal>
 
-        <HelpModal
-          handleClickModalOk={handleClickModalOk}
-          handleClickModalCancel={handleClickModalCancel}
-          isModalOpen={isModalOpen}
-        />
-      </footer>
+      {/* Help modal */}
+      <KiddleModal
+        handleClickModalOk={handleClickHelpOk}
+        handleClickModalCancel={handleClickHelpCancel}
+        isModalOpen={isHelpOpen}
+        title='Help'>
+        <p style={{ fontSize: 'max(32px, 4vmin)' }}>
+          Click <button>Start</button> to play.
+          <br />
+          Type the letters to guess the word before the timer hits 0!
+        </p>
+      </KiddleModal>
     </div>
   )
 }

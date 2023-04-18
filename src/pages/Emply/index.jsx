5de8b2799ae5174b43590328904b66068a67e11b
Leo Wong <leoelicos@gmail.com>
2023-04-18 20:14:30 +1000

    Remove header abstraction. Add logo

--- src/pages/Emply/index.jsx
+++ src/pages/Emply/index.jsx
@@ -1,9 +1,9 @@
 import EmployeeProvider from './context/EmployeeContext'
-import Header from './components/Header'
 import Main from './components/Main'
 
 import useTitle from '../../hooks/useTitle'
 import useFavicon from '../../hooks/useFavicon'
+import logo from './images/emply.png'
 import './style/index.css'
 
 export default function Emply() {
@@ -11,7 +11,13 @@ export default function Emply() {
   useFavicon('/favicons/emply.png')
   return (
     <div className='emply disable-caret'>
-      <Header title='Team Maker' />
+      <header>
+        <img
+          className='logo'
+          src={logo}
+        />
+        <h1>Emply</h1>
+      </header>
       <EmployeeProvider>
         <Main />
       </EmployeeProvider>

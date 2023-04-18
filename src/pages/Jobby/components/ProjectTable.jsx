9c4498ef78a0da4c4be9ef1a9170022123112ce9
Leo Wong <leoelicos@gmail.com>
2023-04-19 03:12:25 +1000

    Fixed dueDate not rendering

--- src/pages/Jobby/components/ProjectTable.jsx
+++ src/pages/Jobby/components/ProjectTable.jsx
@@ -42,44 +42,64 @@ const columns = [
     shortTitle: 'Total ($)',
     dataIndex: 'potentialEarnings'
   },
-  { title: '', shortTitle: '', dataIndex: 'deleteButton' }
+  {
+    title: '',
+    shortTitle: '',
+    dataIndex: 'deleteButton'
+  }
 ]
-const ProjectTable = ({ data, onDelete }) => {
+export default function ProjectTable({ data, onDelete }) {
   const tableRef = useRef(null)
   const w = useResize(tableRef)
 
   const now = new Date().getTime()
-  const deserialized = data.map(({ timestamp, projectName, projectType, hours, hourlyRate, dueDate }) => {
-    return {
-      key: timestamp,
+  const deserialized = data.map(
+    ({
+      timestamp,
       projectName,
       projectType,
       hours,
-      hourlyRate: hourlyRate.toFixed(2),
-      dueDate: new Date(dueDate).toLocaleDateString(),
-      daysUntilDueDate: Math.max(0, Math.floor((new Date(dueDate).getTime() - now) / (1000 * 60 * 60 * 24))),
-      potentialEarnings: (hours * Number(hourlyRate)).toFixed(2),
-      deleteButton: (
-        <Button
-          type='primary'
-          onClick={() => {
-            onDelete(timestamp)
-          }}>
-          <FontAwesomeIcon icon={faTrash} />
-        </Button>
-      )
+      hourlyRate,
+      dueDate //
+    }) => {
+      console.log({ dueDate, type: typeof dueDate })
+      return {
+        key: timestamp,
+        projectName,
+        projectType,
+        hours,
+        hourlyRate: hourlyRate.toFixed(2),
+        dueDate: dueDate.toLocaleDateString(),
+        daysUntilDueDate:
+          dueDate > now //
+            ? ((dueDate - now) / (1000 * 24 * 60 * 60)).toFixed(1)
+            : 0,
+        potentialEarnings: (hours * Number(hourlyRate)).toFixed(2),
+        deleteButton: (
+          <Button
+            type='primary'
+            onClick={() => {
+              onDelete(timestamp)
+            }}>
+            <FontAwesomeIcon icon={faTrash} />
+          </Button>
+        )
+      }
     }
-  })
+  )
   return (
     <Table
       dataSource={deserialized}
       ref={tableRef}>
       {columns
         .filter(({ dataIndex }) => {
-          if (w > 600) return true
-
-          if (dataIndex === 'hourlyRate' || dataIndex === 'hours' || dataIndex === 'dueDate') return false
-          return true
+          if (w > 600) {
+            return true
+          } else if (dataIndex === 'hourlyRate' || dataIndex === 'hours' || dataIndex === 'dueDate') {
+            return false
+          } else {
+            return true
+          }
         })
         .map(({ title, shortTitle, dataIndex }) => (
           <Column
@@ -91,4 +111,3 @@ const ProjectTable = ({ data, onDelete }) => {
     </Table>
   )
 }
-export default ProjectTable

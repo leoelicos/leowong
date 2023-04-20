83bb5ec49d220e871992b68ddce4d76c23218ec5
Leo Wong <leoelicos@gmail.com>
2023-04-21 06:01:38 +1000

    Separate 'featured' and 'other'

--- src/pages/All/index.jsx
+++ src/pages/All/index.jsx
@@ -3,58 +3,12 @@ import Project from '../Home/components/4__Projects/Project.jsx'
 import appData from '../../data/apps.js'
 import './style.css'
 import GradientText from '../../components/GradientText/index.jsx'
-const {
-  ingre,
-  readr,
-  moovee,
-  shopr,
-  submitted,
-  kiddin,
-  locus,
-  spriddle,
-  sytycj,
-  onetwothreego,
-  hourly,
-  emply,
-  docu,
-  txtly,
-  regexpo,
-  algoz,
-  fundr //
-  // taggr,
-  // devtips,
-  // votr,
-  // clist,
-  // vroom,
-  // writr,
-  // depto,
-  // cinem,
-  // wander,
-  // grader,
-  // passo,
-  // megen,
-} = appData
+const { ingre, readr, moovee, shopr, submitted, kiddin, locus, spriddle, sytycj, onetwothreego, hourly, emply, docu, txtly, regexpo, algoz, fundr, kiddle, taggr, devtips, votr, clist, vroom, writr, depto, cinem, wander, grader, passo, megen } = appData
 
 export default function All() {
-  const apps = [
-    ingre,
-    readr,
-    moovee,
-    locus,
-    shopr,
-    spriddle,
-    sytycj,
-    onetwothreego,
-    submitted,
-    kiddin,
-    hourly,
-    emply,
-    fundr,
-    docu,
-    txtly,
-    regexpo,
-    algoz //
-  ]
+  const featured = [ingre, submitted, kiddin, readr, moovee, shopr]
+
+  const other = [kiddle, algoz, spriddle, locus, sytycj, onetwothreego, hourly, emply, fundr, docu, txtly, regexpo]
 
   return (
     <div className='all'>
@@ -66,17 +20,28 @@ export default function All() {
         </GradientText>
       </h1>
 
-      <div className='projects'>
+      <div className='projects featured'>
+        <h2>Featured</h2>
         <div className='project-cards'>
-          {apps.map((project) => (
+          {featured.map((project) => (
             <Project
               key={project.title}
               project={project}
             />
           ))}
         </div>
+      </div>
 
-        <div style={{ height: '36vh' }} />
+      <div className='projects'>
+        <h2>Other</h2>
+        <div className='project-cards'>
+          {other.map((project) => (
+            <Project
+              key={project.title}
+              project={project}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
